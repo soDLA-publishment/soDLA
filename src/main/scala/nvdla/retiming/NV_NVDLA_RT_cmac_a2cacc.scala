22473e4c2da9c467ab976df1087bc220ae7c2bfa
redpanda3 <yuda@berkeley.edu>
2018-10-25 14:51:24 -0700

    ..

--- src/main/scala/nvdla/retiming/NV_NVDLA_RT_cmac_a2cacc.scala
+++ src/main/scala/nvdla/retiming/NV_NVDLA_RT_cmac_a2cacc.scala
@@ -11,28 +11,28 @@ class NV_NVDLA_RT_cmac_a2cacc(implicit val conf: cmacConfiguration) extends Modu
 
         //control signal
         val mac2accu_src_pvld = Input(Bool())
-        val mac2accu_src_mask = Input(UInt(conf.CMAC_ATOMK_HALF).W)
-        val mac2accu_src_mode = Input(UInt(conf.CMAC_ATOMK_HALF).W)
+        val mac2accu_src_mask = Input(UInt((conf.CMAC_ATOMK_HALF).W))
+        val mac2accu_src_mode = Input(UInt((conf.CMAC_ATOMK_HALF).W))
         val mac2accu_src_pd = Input(UInt(9).W)//magic number
 
         val mac2accu_dst_pvld = Output(Bool())
-        val mac2accu_dst_mask = Output(UInt(conf.CMAC_ATOMK_HALF).W)
-        val mac2accu_dst_mode = Output(UInt(conf.CMAC_ATOMK_HALF).W)
-        val mac2accu_dst_pd = Output(UInt(9).W)//magic number
+        val mac2accu_dst_mask = Output(UInt((conf.CMAC_ATOMK_HALF).W))
+        val mac2accu_dst_mode = Output(UInt((conf.CMAC_ATOMK_HALF).W))
+        val mac2accu_dst_pd = Output(UInt(9.W))//magic number
 
 
         //data signal
-        val mac2accu_src_data = Input(Vec(conf.CMAC_ATOMK_HALF, UInt(conf.CMAC_RESULT_WIDTH).W))
-        val mac2accu_dst_data = Output(Vec(conf.CMAC_ATOMK_HALF, UInt(conf.CMAC_RESULT_WIDTH).W))
+        val mac2accu_src_data = Input(Vec(conf.CMAC_ATOMK_HALF, UInt((conf.CMAC_RESULT_WIDTH).W)))
+        val mac2accu_dst_data = Output(Vec(conf.CMAC_ATOMK_HALF, UInt((conf.CMAC_RESULT_WIDTH).W)))
 
 
     })
 
 
-    val mac2accu_data_d = Reg(Vec(conf.RT_CMAC_A2CACC_LATENCY, UInt(conf.CMAC_RESULT_WIDTH).W))
-    val mac2accu_mask_d = Reg(Vec(conf.RT_CMAC_A2CACC_LATENCY, UInt(conf.CMAC_ATOMK_HALF).W))
-    val mac2accu_mode_d = Reg(Vec(conf.RT_CMAC_A2CACC_LATENCY, UInt(conf.CMAC_ATOMK_HALF).W))
-    val mac2accu_pd_d = Reg(Vec(conf.RT_CMAC_A2CACC_LATENCY, UInt(9).W))
+    val mac2accu_data_d = Reg(Vec(conf.RT_CMAC_A2CACC_LATENCY, Vec(conf.CMAC_ATOMK_HALF, UInt((conf.CMAC_RESULT_WIDTH).W))))
+    val mac2accu_mask_d = Reg(Vec(conf.RT_CMAC_A2CACC_LATENCY, UInt((conf.CMAC_ATOMK_HALF).W)))
+    val mac2accu_mode_d = Reg(Vec(conf.RT_CMAC_A2CACC_LATENCY, UInt((conf.CMAC_ATOMK_HALF).W)))
+    val mac2accu_pd_d = Reg(Vec(conf.RT_CMAC_A2CACC_LATENCY, UInt(9.W))
     val mac2accu_pvld_d = Reg(Vec(conf.RT_CMAC_A2CACC_LATENCY, Bool()))
 
 
@@ -47,24 +47,45 @@ class NV_NVDLA_RT_cmac_a2cacc(implicit val conf: cmacConfiguration) extends Modu
     //initial condition
     withClockAndReset(io.nvdla_core_clk, !io.nvdla_core_rstn) {
 
-        mac2accu_pvld_d(1) := RegNext(mac2accu_pvld_d(0))
-        mac2accu_mask_d(0)
-        
+        for(t <- 0 to (conf.RT_CMAC_A2CACC_LATENCY-1){
+
+            mac2accu_pvld_d(t+1) := RegNext(mac2accu_pvld_d(t))
+            mac2accu_mask_d(t+1) := RegNext(mac2accu_mask_d(t))
+  
+        }
     } 
 
     //data flight
     withClock(io.nvdla_core_clk) {
-
         for(t <- 0 to (conf.RT_CMAC_A2CACC_LATENCY-1){
 
             mac2accu_pd_d(t+1) := ShiftRegister(mac2accu_pd_d(t) , 1, mac2accu_pvld_d(t))
             mac2accu_mode_d(t+1) := ShiftRegister(mac2accu_mode_d(t) , 1, mac2accu_pvld_d(t))
-            mac2accu_mask_d(t+1) := ShiftRegister(mac2accu_mask_d(t) , 1, mac2accu_pvld_d(t))
-            when 
-            mac2accu_data_d(t+1) := 
+            
+            for(i <- 0 to (conf.CMAC_ATOMK_HALF-1){
+            when (mac2accu_mask_d(t)(i)){
+
+                    mac2accu_data_d(t+1)(i)(43,0):= RegNext(mac2accu_data_d(t)(i)(43,0))
+                }
+                when (mac2accu_mode_d(t)(i)){
+
+                        mac2accu_data_d(t+1)(i)(conf.CMAC_RESULT_WIDTH,44):= RegNext(mac2accu_data_d(t)(i)(conf.CMAC_RESULT_WIDTH,44))
+
+                }       
+                
+            }
         }
- 
-        
-    }    
+   
+    }  
+
+    //output assignment
+
+    mac2accu_pvld_d(2) := io.mac2accu_dst_pvld
+    mac2accu_mask_d(2) := io.mac2accu_dst_mask
+    mac2accu_mode_d(2) := io.mac2accu_dst_mode
+    mac2accu_pd_d(2) := io.mac2accu_dst_pd
+    mac2accu_data_d(2) := io.mac2accu_dst_data
+
+
 
   }
