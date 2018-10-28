68521238953d1d4b636d9f993c1132dc0d80f5c2
redpanda3 <yuda@berkeley.edu>
2018-10-28 03:05:08 -0700

    ..

--- src/main/scala/nvdla/cmac/NV_NVDLA_CMAC_CORE_active.scala
+++ src/main/scala/nvdla/cmac/NV_NVDLA_CMAC_CORE_active.scala
@@ -111,8 +111,6 @@ class NV_NVDLA_CMAC_CORE_active(implicit val conf: cmacConfiguration) extends Mo
     //: )
 
     io.dat_actv_data := Reg(Vec(conf.CMAC_ATOMK_HALF, UInt((conf.CMAC_BPE*conf.CMAC_ATOMC).W)))
-    val dat_pre_data_w = Wire(UInt((conf.CMAC_BPE*conf.CMAC_ATOMC).W)) 
-    val dat_pre_mask_w = Wire(UInt((conf.CMAC_ATOMC).W)) 
     val dat_pre_nz_w = Reg(UInt((conf.CMAC_ATOMC).W)) 
     io.dat_pre_stripe_end := Reg(Bool()) 
     io.dat_pre_stripe_st := Reg(Bool())
@@ -152,19 +150,14 @@ class NV_NVDLA_CMAC_CORE_active(implicit val conf: cmacConfiguration) extends Mo
 
 
     withClockAndReset(io.nvdla_core_clk, !io.nvdla_core_rstn) {
-        when(io.in_wt_pvld){
-            wt_pre_nz := RegNext(io.in_wt_mask)
-        }
+        when(io.in_wt_pvld) {wt_pre_nz := RegNext(io.in_wt_mask)}
         wt_pre_sel := RegNext(in_wt_sel&Fill(conf.CMAC_ATOMK_HALF, io.in_wt_pvld))
-
     } 
 
 
     withClock(io.nvdla_core_clk) {
-        for(i <- 0 to conf.CMAC_ATOMC-1){
-            when (io.in_wt_pvld & wt_pre_mask_w(i)){
-                wt_pre_data(i*8+7,i*8) :=RegNext(io.in_wt_data(i))
-            }
+        for(i <- 0 to (conf.CMAC_ATOMC-1)){
+            when (io.in_wt_pvld & wt_pre_mask_w(i)) {wt_pre_data(i*8+7,i*8) :=RegNext(io.in_wt_data(i))}
         }
     }
 
@@ -190,30 +183,99 @@ class NV_NVDLA_CMAC_CORE_active(implicit val conf: cmacConfiguration) extends Mo
 
 
     io.wt_sd_pvld := Reg(Vec(conf.CMAC_ATOMK_HALF, Bool())) 
-
     val wt_sd_pvld_w = Wire(Vec(conf.CMAC_ATOMK_HALF, Bool()))
-
     val wt_sd_nz = Reg(Vec(conf.CMAC_ATOMK_HALF, conf.CMAC_ATOMC.W))
-
     for(i <- 0 to conf.CMAC_ATOMK_HALF-1){
-
         wt_sd_pvld_w(i) := Mux(wt_pre_sel(i), true.B, Mux(io.dat_pre_stripe_st, false.B, io.wt_sd_pvld(i)))
-            
-        withClockAndReset(io.nvdla_core_clk, !io.nvdla_core_rstn) {
-
+        withClockAndReset(io.nvdla_core_clk, !io.nvdla_core_rstn) { 
             wt_sd_pvld(i) := RegNext(wt_sd_pvld_w(i))
+            when(wt_pre_sel(i)) {io.wt_sd_nz(i) := RegNext(wt_pre_nz)}
+        }         
+    }
+    withClockAndReset(io.nvdla_core_clk, !io.nvdla_core_rstn) {
 
-            when(wt_pre_sel(i)){
+        val dat_actv_stripe_end = RegNext(io.dat_pre_stripe_end)
+    } 
+ 
+   ////////////////////////////////// handle data ///////////////
+    // data pack
+    //:    print "assign    dat_pre_data_w  = {";
+    //:    for(my $i = CMAC_INPUT_NUM-1; $i >= 0; $i --) {
+    //:        print "in_dat_data${i}";
+    //:        if($i == 0) {
+    //:            print "};\n";
+    //:        } elsif ($i % 8 == 0) {
+    //:            print ",\n                       ";
+    //:        } else {
 
-                io.wt_sd_nz(i) := RegNext(wt_pre_nz)
-            }
+    //:            print ", ";
+    //:        }
+    //:    }
+    // data mask pack
+    //:    print "assign    dat_pre_mask_w = {";
+    //:    for(my $i = CMAC_INPUT_NUM-1; $i >= 0; $i --) {
+    //:        print "in_dat_mask[${i}]";
+    //:        if($i == 0) {
+    //:            print "};\n";
+    //:        } elsif ($i % 8 == 0) {
+    //:            print ",\n                       ";
+    //:        } else {
+    //:            print ", ";
+    //:        }
+    //:    }
 
-        } 
-        
+// 1 pipe for input data
+//: my $kk= CMAC_ATOMC; 
+//: &eperl::flop(" -q  dat_pre_pvld   -d \"in_dat_pvld\"  -clk nvdla_core_clk -rst nvdla_core_rstn "); 
+//: &eperl::flop(" -q  dat_pre_nz     -en \"in_dat_pvld\" -d  \"dat_pre_mask_w\" -wid ${kk} -clk nvdla_core_clk -rst nvdla_core_rstn"); 
 
+    withClockAndReset(io.nvdla_core_clk, !io.nvdla_core_rstn) {
+        io.dat_pre_pvld := RegNext(io.in_dat_pvld)
+        when(io.in_dat_pvld) {val dat_pre_nz = RegNext(io.in_dat_mask) }
+    }    
+
+//:     for (my $i = 0; $i < CMAC_ATOMC; $i ++) {
+//:         my $b0 = $i * 8;
+//:         my $b1 = $i * 8 + 7;
+//: &eperl::flop("-nodeclare -norst -q  dat_pre_data[${b1}:${b0}]  -en \"in_dat_pvld & dat_pre_mask_w[${i}]\" -d  \"dat_pre_data_w[${b1}:${b0}]\" -clk nvdla_core_clk"); 
+//: }
+//: &eperl::flop("-nodeclare -q  dat_pre_stripe_st   -d  \"in_dat_stripe_st & in_dat_pvld\" -clk nvdla_core_clk -rst nvdla_core_rstn "); 
+//: &eperl::flop("-nodeclare -q  dat_pre_stripe_end  -d  \"in_dat_stripe_end & in_dat_pvld \" -clk nvdla_core_clk -rst nvdla_core_rstn "); 
+//:     for(my $i = 0; $i < CMAC_ATOMK_HALF; $i ++) {
+//:         print qq {
+//: assign    dat${i}_pre_pvld       = dat_pre_pvld;
+//: assign    dat${i}_pre_stripe_st  = dat_pre_stripe_st;
+//: assign    dat${i}_pre_stripe_end = dat_pre_stripe_end;
+//:     };
+//: }
+
+    val dat_pre_data = Reg(UInt((conf.CMAC_BPE*conf.CMAC_ATOMC).W))
+    for(i <- 0 to conf.CMAC_ATOMC-1){        
+        withClock(io.nvdla_core_clk) {
+            when(io.in_dat_pvld & in_dat_mask_w(i)) { dat_pre_data(i*8+7,i*8) := io.in_dat_data(i) }
+        }
     }
 
+    withClockAndReset(io.nvdla_core_clk, !io.nvdla_core_rstn) {
+        io.dat_actv_stripe_st := io.in_dat_stripe_st&io.in_dat_pvld
+        io.dat_pre_stripe_end := io.in_dat_stripe_end&io.in_dat_pvld
+    } 
 
+// get data for cmac, 1 pipe.
+//: my $atomc= CMAC_ATOMC; 
+//: for(my $i = 0; $i < CMAC_ATOMK_HALF; $i ++) {
+//:     my $l = $i + 8;
+//:     &eperl::flop(" -q  dat_actv_pvld_reg${i}  -d \"{${atomc}{dat_pre_pvld}}\" -wid ${atomc} -clk nvdla_core_clk -rst nvdla_core_rstn "); 
+//:     &eperl::flop(" -q  dat_actv_nz_reg${i}    -en dat_pre_pvld -d  dat_pre_nz -wid $atomc -clk nvdla_core_clk -rst nvdla_core_rstn"); 
+//:     for(my $k = 0; $k < CMAC_ATOMC; $k ++) {
+//:         my $j = int($k/2);
+//:         my $b0 = $k * 8;
+//:         my $b1 = $k * 8 + 7;
+//:         &eperl::flop("-nodeclare -norst -q  dat_actv_data_reg${i}[${b1}:${b0}]  -en \"dat_pre_pvld & dat_pre_nz[${k}]\" -d  \"dat_pre_data[${b1}:${b0}]\" -clk nvdla_core_clk"); 
+//:     }
+//: }
+
+    
 
 
 
