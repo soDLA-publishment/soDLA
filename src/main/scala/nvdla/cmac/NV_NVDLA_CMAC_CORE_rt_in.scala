88e01c5fea4875eb8937264549bc2143fe456260
redpanda3 <yuda@berkeley.edu>
2018-11-01 18:47:15 -0700

    wash cloth now

--- src/main/scala/nvdla/cmac/NV_NVDLA_CMAC_CORE_rt_in.scala
+++ src/main/scala/nvdla/cmac/NV_NVDLA_CMAC_CORE_rt_in.scala
@@ -90,13 +90,9 @@ class NV_NVDLA_CMAC_CORE_rt_in(implicit val conf: cmacConfiguration) extends Mod
     io.in_wt_mask := Wire(UInt((conf.CMAC_ATOMC).W))
     io.in_wt_pvld := Wire(Bool())
     io.in_wt_sel := Wire(UInt((conf.CMAC_ATOMK_HALF).W))
-    val in_rt_dat_pvld_d0 = Wire(Bool())
-    val in_rt_dat_mask_d0 = Wire(UInt((conf.CMAC_ATOMC).W))
-    val in_rt_dat_pd_d0 = Wire(UInt(9.W))
-    val in_rt_dat_data_d0 = Wire(UInt((conf.CMAC_BPE*conf*CMAC_ATOMC).W)
 
     //: for(my $i=1; $i<CMAC_IN_RT_LATENCY+1; $i++){
-    //: print qq(
+    //: print qq.
     //: reg     in_rt_dat_pvld_d${i};
     //: reg     [CMAC_ATOMC-1:0]   in_rt_dat_mask_d${i};
     //: reg     [8:0]              in_rt_dat_pd_d${i};
@@ -104,7 +100,111 @@ class NV_NVDLA_CMAC_CORE_rt_in(implicit val conf: cmacConfiguration) extends Mod
     //: );
     //: }
 
-    val in_rt_dat_pvld_d = Reg()
+    val in_rt_dat_pvld_d_wire = Wire(Bool())
+    val in_rt_dat_pvld_d_regs = Seq.fill(conf.CMAC_IN_RT_LATENCY)(Reg(Bool()))
+    val in_rt_dat_pvld_d = VecInit(in_rt_dat_pvld_d_wire +: in_rt_dat_pvld_d_regs)
+
+    val in_rt_dat_mask_d_wire = Wire(UInt(conf.CMAC_ATOMC.W))
+    val in_rt_dat_mask_d_regs = Seq.fill(conf.CMAC_IN_RT_LATENCY)(Reg(UInt(conf.CMAC_ATOMC.W)))
+    val in_rt_dat_mask_d = VecInit(in_rt_dat_mask_d_wire +: in_rt_dat_mask_d_regs)
+
+    val in_dat_pd_d_wire = Wire(UInt(9.W))
+    val in_dat_pd_d_regs = Seq.fill(conf.CMAC_IN_RT_LATENCY)(Reg(UInt(9.W)))
+    val in_dat_pd_d = VecInit(in_dat_pd_d_wire +: in_dat_pd_d_regs)
+
+    val in_rt_dat_data_d_wire = Wire(UInt((conf.CMAC_BPE*conf*CMAC_ATOMC).W))
+    val in_rt_dat_data_d_regs = Seq.fill(conf.CMAC_IN_RT_LATENCY)(Reg(UInt((conf.CMAC_BPE*conf*CMAC_ATOMC).W)))  
+    val in_rt_dat_data_d = VecInit(in_rt_dat_data_d_wire +: in_rt_dat_data_d_regs)
+
+
+    //: for(my $i=1; $i<CMAC_IN_RT_LATENCY+1; $i++){
+    //: print qq(
+    //: reg    [CMAC_BPE*CMAC_ATOMC-1:0]    in_rt_wt_data_d${i};
+    //: reg    [CMAC_ATOMC-1:0]             in_rt_wt_mask_d${i};
+    //: reg                                 in_rt_wt_pvld_d${i};
+    //: reg    [CMAC_ATOMK_HALF-1:0]        in_rt_wt_sel_d${i};
+    //: )
+    //: }
+
+
+    val in_rt_wt_data_d_wire = Wire(UInt((conf.CMAC_BPE*conf*CMAC_ATOMC).W))
+    val in_rt_wt_data_d_regs = Seq.fill(conf.CMAC_IN_RT_LATENCY)(Reg(UInt((conf.CMAC_BPE*conf*CMAC_ATOMC).W)))  
+    val in_rt_wt_data_d = VecInit(in_rt_wt_data_d_wire +: in_rt_wt_data_d_regs)
+
+    val in_rt_wt_mask_d_wire = Wire(UInt(conf.CMAC_ATOMC.W))
+    val in_rt_wt_mask_d_regs = Seq.fill(conf.CMAC_IN_RT_LATENCY)(Reg(UInt(conf.CMAC_ATOMC.W)))
+    val in_rt_wt_mask_d = VecInit(in_rt_wt_mask_d_wire +: in_rt_wt_mask_d_regs)
+
+    val in_rt_wt_pvld_d_wire = Wire(Bool())
+    val in_rt_wt_pvld_d_regs = Seq.fill(conf.CMAC_IN_RT_LATENCY)(Reg(Bool()))
+    val in_rt_wt_pvld_d = VecInit(in_rt_wt_pvld_d_wire +: in_rt_wt_pvld_d_regs)
+
+    val in_rt_wt_sel_d_wire = Wire(UInt((conf.CMAC_ATOMK_HALF).W))
+    val in_rt_wt_sel_d_regs = Seq.fill(conf.CMAC_IN_RT_LATENCY)(Reg(UInt((conf.CMAC_ATOMK_HALF).W)))  
+    val in_rt_wt_sel_d = VecInit(in_rt_wt_sel_d_wire +: in_rt_wt_sel_d_regs)
+
+    // assign
+    in_rt_dat_pvld_d(0) := io.sc2mac_dat_pvld
+    in_rt_dat_mask_d(0) := io.sc2mac_dat_mask
+    in_rt_dat_pd_d(0) := io.sc2mac_dat_pd
+    in_rt_wt_pvld_d(0) := io.sc2mac_wt_pvld
+    in_rt_wt_mask_d(0) := io.sc2mac_wt_mask
+    in_rt_wt_sel_d(0) := io.sc2mac_wt_sel
+
+    in_rt_dat_data${k}_d0
+
+    
+
+
+
+
+
+
+
+
+
+    //: for(my $i=1; $i<CMAC_IN_RT_LATENCY+1; $i++){
+    //: print qq(
+    //: reg    [CMAC_BPE*CMAC_ATOMC-1:0]    in_rt_wt_data_d${i};
+    //: reg    [CMAC_ATOMC-1:0]             in_rt_wt_mask_d${i};
+    //: reg                                 in_rt_wt_pvld_d${i};
+    //: reg    [CMAC_ATOMK_HALF-1:0]        in_rt_wt_sel_d${i};
+    //: )
+    //: }
+
+    val in_rt_wt_data_d = Reg(Vec(conf.CMAC_IN_RT_LATENCY, UInt((conf.CMAC_BPE*conf*CMAC_ATOMC).W)))
+    val in_rt_wt_mask_d = Reg(Vec(conf.CMAC_IN_RT_LATENCY, UInt(conf.CMAC_ATOMC)))
+    val in_wr_wt_pvld_d = Reg(Vec(conf.CMAC_IN_RT_LATENCY, Bool()))
+    val in_rt_wt_sel_d = Reg(Vec(conf.CMAC_IN_RT_LATENCY, UInt((conf.CMAC_ATOMK_HALF).W)))
+
+    in_rt_dat_pvld_d0 := io.sc2mac_dat_pvld
+    in_rt_dat_mask_d0 := io.sc2mac_dat_mask
+    in_rt_dat_pd_d0 := io.sc2mac_dat_pd
+    in_rt_wt_pvld_d0 := io.sc2mac_wt_pvld
+    in_rt_wt_mask_d0 := io.sc2mac_wt_mask
+    in_rt_wt_sel_d0 := sc2mac_wt_sel
+
+    //:     my $kk=CMAC_BPE;
+    //:     for(my $k = 0; $k <CMAC_ATOMC; $k ++) {
+    //:         print "wire [$kk-1:0]  in_rt_dat_data${k}_d0 = sc2mac_dat_data${k}; \n"; 
+    //:     }
+    //:     for(my $k = 0; $k <CMAC_ATOMC; $k ++) {
+    //:         print "wire [$kk-1:0]  in_rt_wt_data${k}_d0 = sc2mac_wt_data${k}; \n"; 
+    //:     }
+
+    val in_rt_dat_data_d0_c = Wire(Vec(conf.CMAC_ATOMC, UInt((conf.CMAC_BPE).W)))
+    val in_rt_wt_data_d0_c = Wire(Vec(conf.CMAC_ATOMC, UInt((conf.CMAC_BPE).W)))
+
+
+
+
+
+
+
+
+
+
+
     
 
 
--- src/main/scala/nvdla/retiming/NV_NVDLA_RT_cmac_a2cacc.scala
+++ src/main/scala/nvdla/retiming/NV_NVDLA_RT_cmac_a2cacc.scala
@@ -2,6 +2,8 @@ package nvdla
 
 import chisel3._
 
+//switch to nvdlav2 branch
+
 
 class NV_NVDLA_RT_cmac_a2cacc(implicit val conf: cmacConfiguration) extends Module {
     val io = IO(new Bundle {
@@ -12,12 +14,12 @@ class NV_NVDLA_RT_cmac_a2cacc(implicit val conf: cmacConfiguration) extends Modu
         //control signal
         val mac2accu_src_pvld = Input(Bool())
         val mac2accu_src_mask = Input(UInt((conf.CMAC_ATOMK_HALF).W))
-        val mac2accu_src_mode = Input(UInt((conf.CMAC_ATOMK_HALF).W))
-        val mac2accu_src_pd = Input(UInt(9).W)//magic number
+        val mac2accu_src_mode = Input(Bool())
+        val mac2accu_src_pd = Input(UInt(9.W))//magic number
 
         val mac2accu_dst_pvld = Output(Bool())
         val mac2accu_dst_mask = Output(UInt((conf.CMAC_ATOMK_HALF).W))
-        val mac2accu_dst_mode = Output(UInt((conf.CMAC_ATOMK_HALF).W))
+        val mac2accu_dst_mode = Output(Bool())
         val mac2accu_dst_pd = Output(UInt(9.W))//magic number
 
 
@@ -29,12 +31,37 @@ class NV_NVDLA_RT_cmac_a2cacc(implicit val conf: cmacConfiguration) extends Modu
     })
 
 
-    val mac2accu_data_d = Reg(Vec(conf.RT_CMAC_A2CACC_LATENCY, Vec(conf.CMAC_ATOMK_HALF, UInt((conf.CMAC_RESULT_WIDTH).W))))
-    val mac2accu_mask_d = Reg(Vec(conf.RT_CMAC_A2CACC_LATENCY, UInt((conf.CMAC_ATOMK_HALF).W)))
-    val mac2accu_mode_d = Reg(Vec(conf.RT_CMAC_A2CACC_LATENCY, UInt((conf.CMAC_ATOMK_HALF).W)))
-    val mac2accu_pd_d = Reg(Vec(conf.RT_CMAC_A2CACC_LATENCY, UInt(9.W))
-    val mac2accu_pvld_d = Reg(Vec(conf.RT_CMAC_A2CACC_LATENCY, Bool()))
+    //fix input wire
+    val max2accu_data_d_wire = Wire(Vec(conf.CMAC_ATOMK_HALF, UInt((conf.CMAC_RESULT_WIDTH).W)))
+    val max2accu_data_d_regs = Seq.fill(conf.RT_CMAC_A2CACC_LATENCY)(Reg(Vec(conf.CMAC_ATOMK_HALF, UInt((conf.CMAC_RESULT_WIDTH).W))))
+    val mac2accu_data_d = VecInit(max2accu_data_d_wire +: max2accu_data_d_regs)
 
+    val mac2accu_mask_d_wire = Wire(UInt((conf.CMAC_ATOMK_HALF).W))
+    val mac2accu_mask_d_regs = Seq.fill(conf.RT_CMAC_A2CACC_LATENCY)(Reg(UInt((conf.CMAC_ATOMK_HALF).W)))
+    val mac2accu_data_d = VecInit(mac2accu_mask_d_wire +: mac2accu_mask_d_regs)
+
+    val mac2accu_mode_d_wire = Wire(UInt((conf.CMAC_ATOMK_HALF).W))
+    val mac2accu_mode_d_regs = Seq.fill(conf.RT_CMAC_A2CACC_LATENCY)(Reg(UInt((conf.CMAC_ATOMK_HALF).W)))
+    val mac2accu_mode_d = VecInit(mac2accu_mode_d_wire +: mac2accu_mode_d_regs)  
+
+    val mac2accu_pd_d_wire = Wire(UInt(9.W))
+    val mac2accu_pd_d_regs = Seq.fill(conf.RT_CMAC_A2CACC_LATENCY)(Reg(UInt(9.W)))   
+    val mac2accu_pd_d = VecInit(mac2accu_pd_d_wire +: mac2accu_pd_d_regs)  
+
+    val mac2accu_pvld_d_wire = Wire(Bool())
+    val mac2accu_pvld_d_regs = Seq.fill(conf.RT_CMAC_A2CACC_LATENCY)(Reg(Bool()))   
+    val mac2accu_pvld_d = VecInit(mac2accu_pd_d_wire +: mac2accu_pd_d_regs)  
+    
+    //:    my $delay = RT_CMAC_A2CACC_LATENCY;
+    //:    my $i;
+    //:    my $j;
+    //:    my $k;
+    //:    my $kk=CMAC_ATOMK_HALF;
+    //:    my $jj=CMAC_RESULT_WIDTH;
+    //:    for($k = 0; $k <CMAC_ATOMK_HALF; $k ++) {
+    //:        print "assign mac2accu_data${k}_d0 = mac2accu_src_data${k};\n";
+    //:    }
+    //:
 
     //assign input port
     mac2accu_pvld_d(0) := io.mac2accu_src_pvld
@@ -44,47 +71,49 @@ class NV_NVDLA_RT_cmac_a2cacc(implicit val conf: cmacConfiguration) extends Modu
     mac2accu_data_d(0) := io.mac2accu_src_data
 
 
-    //initial condition
-    withClockAndReset(io.nvdla_core_clk, !io.nvdla_core_rstn) {
+    //:    for($i = 0; $i < $delay; $i ++) {
+    //:        $j = $i + 1;
+    //:        &eperl::flop("-q mac2accu_pvld_d${j} -d mac2accu_pvld_d${i}");
+    //:        &eperl::flop("-wid 9 -q mac2accu_pd_d${j} -en mac2accu_pvld_d${i} -d  mac2accu_pd_d${i}");
+    //:        &eperl::flop("-q mac2accu_mode_d${j} -en mac2accu_pvld_d${i} -d  mac2accu_mode_d${i}");
+    //:        &eperl::flop("-wid ${kk} -q mac2accu_mask_d${j} -d mac2accu_mask_d${i}");
+    //:        for($k = 0; $k < CMAC_ATOMK_HALF; $k ++) {
+    //:            &eperl::flop("-wid ${jj} -q mac2accu_data${k}_d${j} -en mac2accu_mask_d${i}[${k}] -d  mac2accu_data${k}_d${i}");
+    //:        }
+    //:    }
+    //:
 
-        for(t <- 0 to (conf.RT_CMAC_A2CACC_LATENCY-1){
-
-            mac2accu_pvld_d(t+1) := RegNext(mac2accu_pvld_d(t))
-            mac2accu_mask_d(t+1) := RegNext(mac2accu_mask_d(t))
-  
-        }
-    } 
 
     //data flight
+
+    
     withClock(io.nvdla_core_clk) {
         for(t <- 0 to (conf.RT_CMAC_A2CACC_LATENCY-1){
 
-            mac2accu_pd_d(t+1) := ShiftRegister(mac2accu_pd_d(t) , 1, mac2accu_pvld_d(t))
-            mac2accu_mode_d(t+1) := ShiftRegister(mac2accu_mode_d(t) , 1, mac2accu_pvld_d(t))
+            mac2accu_pvld_d(t+1) := mac2accu_pvld_d(t)
+            mac2accu_mask_d(t+1) := mac2accu_mask_d(t)
+
+            when(mac2accu_pvld_d(t)){
+                mac2accu_pd_d(t+1) := mac2accu_pd_d(t)
+                mac2accu_mode_d(t+1) := mac2accu_mode_d(t) 
+            }
             
             for(i <- 0 to (conf.CMAC_ATOMK_HALF-1){
-            when (mac2accu_mask_d(t)(i)){
 
-                    mac2accu_data_d(t+1)(i)(43,0):= RegNext(mac2accu_data_d(t)(i)(43,0))
-                }
-                when (mac2accu_mode_d(t)(i)){
-
-                        mac2accu_data_d(t+1)(i)(conf.CMAC_RESULT_WIDTH,44):= RegNext(mac2accu_data_d(t)(i)(conf.CMAC_RESULT_WIDTH,44))
-
-                }       
-                
+                when (mac2accu_mask_d(t)(i)){
+                    mac2accu_data_d(t+1)(i):= mac2accu_data_d(t)(i)
+                }               
             }
-        }
-   
+        }  
     }  
 
     //output assignment
 
-    mac2accu_pvld_d(2) := io.mac2accu_dst_pvld
-    mac2accu_mask_d(2) := io.mac2accu_dst_mask
-    mac2accu_mode_d(2) := io.mac2accu_dst_mode
-    mac2accu_pd_d(2) := io.mac2accu_dst_pd
-    mac2accu_data_d(2) := io.mac2accu_dst_data
+    io.mac2accu_dst_pvld = mac2accu_pvld_d(conf.RT_CMAC_A2CACC_LATENCY)
+    io.mac2accu_dst_mask = mac2accu_dst_mask_d(conf.RT_CMAC_A2CACC_LATENCY) 
+    io.mac2accu_dst_mode = mac2accu_dst_mode_d(conf.RT_CMAC_A2CACC_LATENCY)
+    io.mac2accu_dst_pd = mac2accu_dst_pd_d(conf.RT_CMAC_A2CACC_LATENCY)
+    io.mac2accu_dst_data = mac2accu_dst_data_d(conf.RT_CMAC_A2CACC_LATENCY)
 
 
 
