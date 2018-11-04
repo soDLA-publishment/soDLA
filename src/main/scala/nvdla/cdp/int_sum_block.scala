70fd6c13e4f54deda70c23ce0481353bd5d1d432
redpanda3 <yuda@berkeley.edu>
2018-11-04 02:27:35 -0800

    add int_sum block

--- src/main/scala/nvdla/cdp/int_sum_block.scala
+++ src/main/scala/nvdla/cdp/int_sum_block.scala
@@ -11,42 +11,102 @@ import common._
 // 9 digit
 
 
-class int_sum_block(pINT8_BW: Int = 16) extends Module {
+class int_sum_block extends Module {
     val io = IO(new Bundle {
         //nvdla core clock
         val nvdla_core_clk = Input(Clock())
         val nvdla_core_rstn = Input(Bool())
 
         //control signal
-        val int8_en = Input(Bool())
         val len5 = Input(Bool())
         val len7 = Input(Bool())
         val len9 = Input(Bool())
         val load_din_d = Input(Bool())
         val load_din_2d = Input(Bool())
-        val reg2dp_normalz_len = Input(UInt(2))
+        val reg2dp_normalz_len = Input(UInt(2.W))
 
         //sq_pd as a input
-
-        val sq_pd = IO(new Bundle{
-            val int16 = Input(Vec(input_num, SInt((pINT8_BW*2+1).W)))
-            val sq_pd_int8 = IO(new Bundle{
-                val lsb = Input(Vec(input_num, SInt((pINT8_BW+1).W)))
-                val msb = Input(Vec(input_num, SInt((pINT8_BW+1).W)))
-            })
-        })
+        val sq_pd_int8_lsb = Input(Vec(9, UInt(17.W)))
+        val sq_pd_int8_msb = Input(Vec(9, UInt(17.W)))
 
         //output signal
-        val int8_sum = Output(SInt(pINT8_BW+1).W))
-        val int16_sum = Output(SInt((pINT8_BW*2+1+input_num/2).W))
+        val int8_sum = Output(UInt(42.W))
     })
 
-        
-    val io = if(int8_en)  else 
+    val int16_sum3 = Reg(UInt(35.W))
+    val int16_sum5 = Reg(UInt(36.W))
+    val int16_sum7 = Reg(UInt(36.W))
+    val int16_sum9 = Reg(UInt(37.W))
+    val int16_sum_0_8 = Reg(UInt(34.W))
+    val int16_sum_1_7 = Reg(UInt(34.W))
+    val int16_sum_2_6 = Reg(UInt(34.W))
+    val int16_sum_3_5 = Reg(UInt(34.W))
+    val int8_lsb_sum = Reg(UInt(21.W))
+    val int8_msb_sum = Reg(UInt(21.W))
+    val int8_msb_sum3 = Reg(UInt(19.W))
+    val int8_msb_sum5 = Reg(UInt(20.W))
+    val int8_msb_sum7 = Reg(UInt(20.W))
+    val int8_msb_sum9 = Reg(UInt(21.W))
+    val int8_msb_sum_0_8 = Reg(UInt(18.W))
+    val int8_msb_sum_1_7 = Reg(UInt(18.W))
+    val int8_msb_sum_2_6 = Reg(UInt(18.W))
+    val int8_msb_sum_3_5 = Reg(UInt(18.W))
+    val sq4_d = Reg(UInt(33.W))
+    val sq_pd_int8_msb_4_d = Reg(UInt(17.W))
 
-    withClockAndReset(io.nvdla_core_clk, !io.nvdla_core_rstn) {
-        when (load_din_d) {int8_msb_sum_3_5 := }
-        
+    val int8_lsb_sum3 = Wire(UInt(19.W))
+    val int8_lsb_sum5 = Wire(UInt(20.W))
+    val int8_lsb_sum7 = Wire(UInt(20.W))
+    val int8_lsb_sum9 = Wire(UInt(21.W))
+
+    val sq = Wire(Vec(9, UInt(33.W)))
+
+    for(i <- 0 to 8){
+        sq(i) := Cat("d0".U(16.W), io.sq_pd_int8_lsb(i))
     }
+
+    int8_lsb_sum3 := int16_sum3(18,0)
+    int8_lsb_sum5 := int16_sum5(19,0)
+    int8_lsb_sum7 := int16_sum5(19,0)
+    int8_lsb_sum9 := int16_sum5(19,0)
+
+
+
+
+   //sum process
+
+   withClockAndReset(io.nvdla_core_clk, !io.nvdla_core_rstn){
+        when(load_din_d){
+            int8_sum_3_5:=io.sq_pd_int8_msb(3) + io.sq_pd_int8_msb(5)
+        }
+        when(load_din_d & (len5|len7|len9)){
+            int8_msb_sum_2_6:=io.sq_pd_int8_msb(2) + io.sq_pd_int8_msb(6)
+        }
+        when(load_din_d & (len7|len9)){
+            int8_msb_sum_1_7 := io.sq_pd_int8_msb(1) + io.sq_pd_int8_msb(7)
+        }
+        when(load_din_d & (len9)){
+            int8_msb_sum_0_8 := io.sq_pd_int8_msb(0) + io.sq_pd_int8_msb(8)
+        }
+        when(load_din_d){
+            int16_sum_3_5:= sq(3) + sq(5)
+        }
+        when(load_din_d & (len5|len7|len9)){
+            int16_sum_2_6 := sq(2) + sq(6)
+        }
+        when(load_din_d & (len7|len9)){
+            int16_sum_1_7 := sq(1) + sq(7)
+        }
+        when(load_din_d & (len9)){
+            int16_sum_0_8 := sq(0) + sq(8)
+        }
+        when(load_din_d){
+            sq4_d:=Cat("d0".U(16.W), io.sq_pd_int8_lsb(4))
+        }
+        when(load_din_d){
+            sq_pd_int8_msb_4_d:=io.sq_pd_int8_msb(4)
+        }
+
+   }
   
 }
