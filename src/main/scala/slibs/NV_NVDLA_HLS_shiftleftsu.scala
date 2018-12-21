5763bf3b9f186433fb4ccfe43ed81d07d1e63fdb
redpanda3 <yuda@berkeley.edu>
2018-12-21 15:51:34 -0800

    Update NV_NVDLA_HLS_shiftleftsu
    module cmd26WrapperHelpertestModule( // @[:@3.2]
      input         clock, // @[:@4.4]
      input         reset, // @[:@5.4]
      input  [15:0] io_data_in, // @[:@6.4]
      input  [5:0]  io_shift_num, // @[:@6.4]
      output [31:0] io_data_out // @[:@6.4]
    );
      wire  data_sign; // @[cmd26.sc 18:25:@11.4]
      wire [62:0] _T_19; // @[Bitwise.scala 72:12:@15.4]
      wire [78:0] _T_20; // @[Cat.scala 30:58:@16.4]
      wire [141:0] _GEN_0; // @[cmd26.sc 23:63:@17.4]
      wire [141:0] _T_21; // @[cmd26.sc 23:63:@17.4]
      wire [46:0] data_high; // @[cmd26.sc 23:79:@18.4]
      wire [31:0] data_shift; // @[cmd26.sc 25:80:@24.4]
      wire [78:0] _T_30; // @[Cat.scala 30:58:@26.4]
      wire [47:0] _T_34; // @[Bitwise.scala 72:12:@28.4]
      wire [78:0] _GEN_2; // @[cmd26.sc 27:50:@29.4]
      wire  left_shift_sat; // @[cmd26.sc 27:50:@29.4]
      wire [31:0] data_max; // @[cmd26.sc 29:20:@36.4]
      wire [31:0] _T_53; // @[cmd26.sc 31:23:@38.4]
      assign data_sign = io_data_in[0]; // @[cmd26.sc 18:25:@11.4]
      assign _T_19 = data_sign ? 63'h7fffffffffffffff : 63'h0; // @[Bitwise.scala 72:12:@15.4]
      assign _T_20 = {_T_19,io_data_in}; // @[Cat.scala 30:58:@16.4]
      assign _GEN_0 = {{63'd0}, _T_20}; // @[cmd26.sc 23:63:@17.4]
      assign _T_21 = _GEN_0 << io_shift_num; // @[cmd26.sc 23:63:@17.4]
      assign data_high = _T_21[78:32]; // @[cmd26.sc 23:79:@18.4]
      assign data_shift = _T_21[31:0]; // @[cmd26.sc 25:80:@24.4]
      assign _T_30 = {data_high,data_shift}; // @[Cat.scala 30:58:@26.4]
      assign _T_34 = data_sign ? 48'hffffffffffff : 48'h0; // @[Bitwise.scala 72:12:@28.4]
      assign _GEN_2 = {{31'd0}, _T_34}; // @[cmd26.sc 27:50:@29.4]
      assign left_shift_sat = _T_30 != _GEN_2; // @[cmd26.sc 27:50:@29.4]
      assign data_max = data_sign ? 32'h80000000 : 32'h0; // @[cmd26.sc 29:20:@36.4]
      assign _T_53 = left_shift_sat ? data_max : data_shift; // @[cmd26.sc 31:23:@38.4]
      assign io_data_out = _T_53;
    endmodule

--- src/main/scala/slibs/NV_NVDLA_HLS_shiftleftsu.scala
+++ src/main/scala/slibs/NV_NVDLA_HLS_shiftleftsu.scala
@@ -4,12 +4,39 @@ import chisel3._
 import chisel3.experimental._
 
 
-class BlackBoxRealAdd extends BlackBox with HasBlackBoxResource {
-  val io = IO(new Bundle() {
-    val in1 = Input(UInt(64.W))
-    val in2 = Input(UInt(64.W))
-    val out = Output(UInt(64.W))
-  })
-  setResource("/real_math.v")
+class NV_NVDLA_HLS_shiftleftsu extends Module {
+    
+    val IN_WIDTH = 16
+    val OUT_WIDTH = 32
+    val SHIFT_WIDTH = 6
+    val SHIFT_MAX = Math.pow(2, SHIFT_WIDTH).toInt - 1
+    val HIGH_WIDTH = SHIFT_MAX + IN_WIDTH - OUT_WIDTH
+    
+    val io = IO(new Bundle {      
+        val data_in = Input(UInt(IN_WIDTH.W))
+        val shift_num = Input(UInt(SHIFT_WIDTH.W))
+        val data_out = Output(UInt(OUT_WIDTH.W))
+    })
+        
+    val data_high = Wire(UInt(HIGH_WIDTH.W))
+    val data_shift = Wire(UInt(OUT_WIDTH.W))
+    val data_max = Wire(UInt(OUT_WIDTH.W))
+    val data_sign = Wire(Bool())
+    val left_shift_sat = Wire(Bool())
+    
+    data_sign := io.data_in
+    
+    data_high := (Cat(Fill(SHIFT_MAX, data_sign), io.data_in) << io.shift_num)(HIGH_WIDTH + OUT_WIDTH - 1, OUT_WIDTH)
+    
+    data_shift := (Cat(Fill(SHIFT_MAX, data_sign), io.data_in) << io.shift_num)(OUT_WIDTH - 1, 0)
+    
+    left_shift_sat := Cat(data_high, data_shift) != Fill(HIGH_WIDTH+1, data_sign)
+    
+    data_max := Mux(data_sign, Cat(true.B, Fill(OUT_WIDTH-1, false.B)), !Cat(true.B, Fill(OUT_WIDTH-1, false.B)))
+    
+    io.data_out := Mux(left_shift_sat, data_max, data_shift)
+    
+
+       
 }
 
