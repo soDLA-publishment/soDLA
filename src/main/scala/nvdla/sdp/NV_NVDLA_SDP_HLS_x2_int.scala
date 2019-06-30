2f8e2c13ec3be48344530f8da9f1a3bcf381dc37
redpanda3 <yuda@berkeley.edu>
2019-06-29 21:33:42 -0700

    6/29/2019

--- src/main/scala/nvdla/sdp/NV_NVDLA_SDP_HLS_x2_int.scala
+++ src/main/scala/nvdla/sdp/NV_NVDLA_SDP_HLS_x2_int.scala
@@ -4,7 +4,7 @@
 // import chisel3.experimental._
 // import chisel3.util._
 
-// class NV_NVDLA_SDP_HLS_x2_int extends Module {
+// class NV_NVDLA_SDP_HLS_x2_int(implicit conf: sdpConfiguration) extends Module {
 //    val io = IO(new Bundle {
 //         val nvdla_core_clk = Input(Clock())
 
@@ -19,16 +19,23 @@
 //         val cfg_mul_shift_value = Input(UInt(6.W))
 //         val cfg_mul_src = Input(Bool())
 //         val cfg_relu_bypass = Input(Bool())
-// modify yourself
+
+//         //chn_alu_op
 //         val chn_alu_op_pvld = Input(Bool())
-// modify yourself
+//         val chn_alu_op_prdy = Output(Bool())
+//         val chn_alu_op = Input(UInt(conf.BN_OP_DW.W))
+
+//         //chn_in
 //         val chn_in_pvld = Input(Bool())
-// modify yourself
+//         val chn_in_prdy = Output(Bool())
+//         val chn_data_in = Input(UInt(conf.BN_IN_DW.W))
+
+
 //         val chn_mul_op_pvld = Input(Bool())
 //         val chn_out_prdy = Input(Bool())
 //         val chn_alu_op_prdy = Output(Bool())
 // modify yourself
-//         val chn_in_prdy = Output(Bool())
+        
 //         val chn_mul_op_prdy = Output(Bool())
 //         val chn_out_pvld = Output(Bool())
 //     })
