839576188943288ffa604f65ff6644fe5921883c
redpanda3 <yuda@berkeley.edu>
2019-07-02 18:40:02 -0700

    7/2/2019

--- src/main/scala/nvdla/sdp/NV_NVDLA_SDP_MRDMA_EG_dout.scala
+++ src/main/scala/nvdla/sdp/NV_NVDLA_SDP_MRDMA_EG_dout.scala
@@ -4,20 +4,12 @@ import chisel3._
 import chisel3.experimental._
 import chisel3.util._
 
-class NV_NVDLA_SDP_MRDMA_EG_dout(implicit val conf: sdpConfiguration) extends Module {
+class NV_NVDLA_SDP_MRDMA_EG_dout(implicit val conf: sdpConfiguration) extends Module{
    val io = IO(new Bundle {
         val nvdla_core_clk = Input(Clock())
 
         val op_load = Input(Bool())
         val eg_done = Output(Bool())
-
-        val reg2dp_height = Input(UInt(13.W))
-        val reg2dp_width = Input(UInt(13.W))
-        val reg2dp_in_precision = Input(UInt(2.W))
-        val reg2dp_proc_precision = Input(UInt(2.W))
-        val reg2dp_perf_nan_inf_count_en = Input(Bool())
-        val dp2reg_status_inf_input_num = Output(UInt(32.W))
-        val dp2reg_status_nan_input_num = Output(UInt(32.W))
 
         val sdp_mrdma2cmux_valid = Output(Bool())
         val sdp_mrdma2cmux_ready = Input(Bool())
@@ -27,21 +19,18 @@ class NV_NVDLA_SDP_MRDMA_EG_dout(implicit val conf: sdpConfiguration) extends Mo
         val cmd2dat_dma_prdy = Output(Bool())
         val cmd2dat_dma_pd = Input(UInt(15.W))
 
-        val pfifo0_rd_pvld = Input(Bool())
-        val pfifo0_rd_prdy = Output(Bool())
-        val pfifo0_rd_pd = Input(UInt(conf.AM_DW.W))
+        val pfifo_rd_pvld = Input(Vec(4, Bool()))
+        val pfifo_rd_prdy = Output(Vec(4, Bool()))
+        val pfifo_rd_pd = Input(Vec(4, UInt(conf.AM_DW.W)))
+        
+        val reg2dp_height = Input(UInt(13.W))
+        val reg2dp_width = Input(UInt(13.W))
+        val reg2dp_in_precision = Input(UInt(2.W))
+        val reg2dp_proc_precision = Input(UInt(2.W))
+        val reg2dp_perf_nan_inf_count_en = Input(Bool())
+        val dp2reg_status_inf_input_num = Output(UInt(32.W))
+        val dp2reg_status_nan_input_num = Output(UInt(32.W))
 
-        val pfifo1_rd_pvld = Input(Bool())
-        val pfifo1_rd_prdy = Output(Bool())
-        val pfifo1_rd_pd = Input(UInt(conf.AM_DW.W))
-
-        val pfifo2_rd_pvld = Input(Bool())
-        val pfifo2_rd_prdy = Output(Bool())
-        val pfifo2_rd_pd = Input(UInt(conf.AM_DW.W))
-
-        val pfifo3_rd_pvld = Input(Bool())
-        val pfifo3_rd_prdy = Output(Bool())
-        val pfifo3_rd_pd = Input(UInt(conf.AM_DW.W))
     })
     //     
     //          ┌─┐       ┌─┐
@@ -65,21 +54,18 @@ class NV_NVDLA_SDP_MRDMA_EG_dout(implicit val conf: sdpConfiguration) extends Mo
     //             │ ─┤ ─┤       │ ─┤ ─┤         
     //             └──┴──┘       └──┴──┘ 
 withClock(io.nvdla_core_clk){
-
-//==============
-// CFG
-//==============
-
-    val cfg_di_int8 = io.reg2dp_in_precision == false.B
-    val cfg_di_int16 = io.reg2dp_in_precision == true.B
-    val cfg_di_fp16 = io.reg2dp_in_precision == 2.U
+    //==============
+    // CFG
+    //==============
+    val cfg_di_int8 = io.reg2dp_in_precision === 0.U
+    val cfg_di_int16 = io.reg2dp_in_precision === 1.U
+    val cfg_di_fp16 = io.reg2dp_in_precision === 2.U
     val cfg_di_16 = cfg_di_int16 | cfg_di_fp16
-    val cfg_do_int8 = io.reg2dp_proc_precision == true.B
-// #ifdef NVDLA_SDP_DATA_TYPE_INT16TO8
-    val cfg_mode_1x1_pack = (io.reg2dp_width == false.B) & (io.reg2dp_height == false.B)
+    val cfg_do_int8 = io.reg2dp_proc_precision === 0.U
+    val cfg_mode_1x1_pack = (io.reg2dp_width === 0.U) & (io.reg2dp_height === 0.U)
     val cfg_perf_nan_inf_count_en = io.reg2dp_perf_nan_inf_count_en
 
-//pop command dat fifo //
+    //pop command dat fifo //
     val dat_accept = Wire(Bool())
     val is_last_beat = Wire(Bool())
     val fifo_vld = Wire(Bool())
@@ -89,12 +75,10 @@ withClock(io.nvdla_core_clk){
 
     val cmd2dat_dma_size = io.cmd2dat_dma_pd(13, 0)
     val cmd2dat_dma_cube_end  = io.cmd2dat_dma_pd(14)
-
     val size_of_beat = (Fill(14, io.cmd2dat_dma_pvld)) & cmd2dat_dma_size
     val cmd_cube_end = io.cmd2dat_dma_pvld & cmd2dat_dma_cube_end
 
     val beat_cnt = RegInit(0.U(14.W))
-
     when(dat_accept){
         when(is_last_beat){
             beat_cnt := 0.U
@@ -104,72 +88,73 @@ withClock(io.nvdla_core_clk){
     }
     is_last_beat := (beat_cnt === size_of_beat)
 
-    // #ifdef NVDLA_SDP_DATA_TYPE_INT16TO8
-    // #else
-
-    val pfifo0_sel = beat_cnt(1, 0) === 0.U
-    val pfifo1_sel = beat_cnt(1, 0) === 1.U
-    val pfifo2_sel = beat_cnt(1, 0) === 2.U
-    val pfifo3_sel = beat_cnt(1, 0) === 3.U
-
-    val pfifo_vld = (io.pfifo3_rd_pvld & pfifo3_sel) | 
-                    (io.pfifo2_rd_pvld & pfifo2_sel) | 
-                    (io.pfifo1_rd_pvld & pfifo1_sel) | 
-                    (io.pfifo0_rd_pvld & pfifo0_sel)
-
-    // #ifdef NVDLA_SDP_DATA_TYPE_INT16TO8
-    // #else
+    val pfifo_sel = Wire(Vec(4, Bool()))
+    val pfifo_rd_sel_pvld = Wire(Vec(4, Bool()))
+    for(i <- 0 to 3){
+        pfifo_sel(i) := beat_cnt(1, 0) === i.U
+        pfifo_rd_sel_pvld(i) := io.pfifo_rd_pvld(i)&pfifo_sel(i)
+    }
+    val pfifo_vld = pfifo_rd_sel_pvld.asUInt.orR
 
     fifo_vld := pfifo_vld
+    val dat_vld = fifo_vld  //& cmd2dat_dma_pvld;
 
-    val dat_vld = fifo_vld
+    val pfifo_rd_data = Wire(Vec(4, UInt(conf.AM_DW.W)))
+    for(i <- 0 to 3){
+        io.pfifo_rd_prdy(i) := dat_rdy & pfifo_sel(i)
+        pfifo_rd_data(i) := (Fill(conf.AM_DW, pfifo_sel(i))) & io.pfifo_rd_pd(i)
+    }
 
-    io.pfifo0_rd_prdy := dat_rdy & pfifo0_sel
-    io.pfifo1_rd_prdy := dat_rdy & pfifo1_sel
-    io.pfifo2_rd_prdy := dat_rdy & pfifo2_sel
-    io.pfifo3_rd_prdy := dat_rdy & pfifo3_sel
+    val pfifo_data_byte_16 = Wire(Vec(4, Vec(conf.NVDLA_MEMORY_ATOMIC_SIZE/2, UInt(16.W))))
+    val pfifo_data_ext_byte_int16 = Wire(Vec(4, Vec(conf.NVDLA_MEMORY_ATOMIC_SIZE/2, UInt(32.W))))
+    for(i <- 0 to 3){
+        for(j <- 0 to conf.NVDLA_MEMORY_ATOMIC_SIZE/2 - 1){
+            pfifo_data_byte_16(i)(j) := pfifo_rd_data(i)(16*j+15, 16*j)
+            pfifo_data_ext_byte_int16(i)(j) := Cat(Fill(16, pfifo_data_byte_16(i)(j)(15)), pfifo_data_byte_16(i)(j))
+        }
+    }
 
-    val pfifo0_rd_data = (Fill(conf.AM_DW, pfifo0_sel)) & io.pfifo0_rd_pd
-    val pfifo1_rd_data = (Fill(conf.AM_DW, pfifo1_sel)) & io.pfifo1_rd_pd
-    val pfifo2_rd_data = (Fill(conf.AM_DW, pfifo2_sel)) & io.pfifo2_rd_pd
-    val pfifo3_rd_data = (Fill(conf.AM_DW, pfifo3_sel)) & io.pfifo3_rd_pd
+    val pfifo_data_ext_byte_16 = pfifo_data_ext_byte_int16
+    val pfifo_data_16 = Wire(Vec(4, UInt(conf.DP_DIN_DW.W)))
+    for(i <- 0 to 3){
+        pfifo_data_16(i) := pfifo_data_ext_byte_16(i).asUInt
+    }
 
-    // #ifdef NVDLA_FEATURE_DATA_TYPE_FP16
+    //// int8 ///////////
+    val pfifo_data_byte_8 = Wire(Vec(4, Vec(conf.NVDLA_MEMORY_ATOMIC_SIZE, UInt(8.W))))
+    val pfifo_data_ext_byte_8 = Wire(Vec(4, Vec(conf.NVDLA_MEMORY_ATOMIC_SIZE, UInt(32.W))))
+    for(i <- 0 to 3){
+        for(j <- 0 to conf.NVDLA_MEMORY_ATOMIC_SIZE - 1){
+            pfifo_data_byte_8(i)(j) := pfifo_rd_data(i)(8*j+7, 8*j)
+            pfifo_data_ext_byte_8(i)(j) := Cat(Fill(24, pfifo_data_byte_8(i)(j)(7)), pfifo_data_byte_8(i)(j))
+        }
+    }
 
-//=====PERF COUNT BEG=============
-    // #ifdef NVDLA_FEATURE_DATA_TYPE_FP16
-    // #else
+    // INT8, concate
+    val pfifo_data_8 = Wire(Vec(4, UInt(conf.DP_DIN_DW.W)))
+    for(i <- 0 to 3){
+        pfifo_data_8(i) := pfifo_data_ext_byte_8(i).asUInt
+    }
+
+    //=====PERF COUNT BEG============= 
     io.dp2reg_status_inf_input_num := 0.U
     io.dp2reg_status_nan_input_num := 0.U
 
-    // #ifdef NVDLA_SDP_DATA_TYPE_INT16TO8
-
-    // #ifdef NVDLA_SDP_DATA_TYPE_INT16TO8
-    // #else
-    val pfifo_data_r = Reg(UInt(conf.DP_DIN_DW.W))
-
-    val pfifo_data0_16 = Wire(UInt(conf.DP_DIN_DW.W))
-    val pfifo_data1_16 = Wire(UInt(conf.DP_DIN_DW.W))
-    val pfifo_data2_16 = Wire(UInt(conf.DP_DIN_DW.W))
-    val pfifo_data3_16 = Wire(UInt(conf.DP_DIN_DW.W))
-    val pfifo_data0_8 = Wire(UInt(conf.DP_DIN_DW.W))
-    val pfifo_data1_8 = Wire(UInt(conf.DP_DIN_DW.W))
-    val pfifo_data2_8 = Wire(UInt(conf.DP_DIN_DW.W))
-    val pfifo_data3_8 = Wire(UInt(conf.DP_DIN_DW.W))
-    pfifo_data_r := MuxCase(
-        Fill(conf.DP_DIN_DW, false.B),
+    //=====PERF COUNT END=============
+    val pfifo_data_r = MuxCase(
+        0.U,
         Array(
-            (pfifo0_sel === true.B) -> Mux(cfg_di_16.asBool, pfifo_data0_16, pfifo_data0_8),
-            (pfifo1_sel === true.B) -> Mux(cfg_di_16.asBool, pfifo_data1_16, pfifo_data1_8),
-            (pfifo2_sel === true.B) -> Mux(cfg_di_16.asBool, pfifo_data2_16, pfifo_data2_8),
-            (pfifo3_sel === true.B) -> Mux(cfg_di_16.asBool, pfifo_data3_16, pfifo_data3_8)
+            (pfifo_sel(0) === true.B) -> Mux(cfg_di_16, pfifo_data_16(0), pfifo_data_8(0)),
+            (pfifo_sel(1) === true.B) -> Mux(cfg_di_16, pfifo_data_16(1), pfifo_data_8(1)),
+            (pfifo_sel(2) === true.B) -> Mux(cfg_di_16, pfifo_data_16(2), pfifo_data_8(2)),
+            (pfifo_sel(3) === true.B) -> Mux(cfg_di_16, pfifo_data_16(3), pfifo_data_8(3))
         )
     )
     val dat_data = pfifo_data_r
 
     dat_accept := dat_vld & dat_rdy
     val dat_layer_end = cmd_cube_end & is_last_beat
-    val dat_batch_end = dat_layer_end
+    val dat_batch_end = dat_layer_end & is_last_beat
 
     val dat_pd = Cat(dat_layer_end, dat_batch_end, dat_data)
 
@@ -183,11 +168,9 @@ withClock(io.nvdla_core_clk){
     io.sdp_mrdma2cmux_pd := pipe_p1.io.dout
 
     val sdp_mrdma2cmux_layer_end = io.sdp_mrdma2cmux_pd(conf.DP_DIN_DW+1)
-    io.eg_done := RegInit(false.B)
-    io.eg_done := sdp_mrdma2cmux_layer_end & io.sdp_mrdma2cmux_valid & io.sdp_mrdma2cmux_ready
+    io.eg_done := RegNext(sdp_mrdma2cmux_layer_end & io.sdp_mrdma2cmux_valid & io.sdp_mrdma2cmux_ready, false.B)
 
-}
-}
+}}
 
 object NV_NVDLA_SDP_MRDMA_EG_doutDriver extends App {
     implicit val conf: sdpConfiguration = new sdpConfiguration
