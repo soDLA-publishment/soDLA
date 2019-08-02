80933dbaa4cc7951b8d54bd32459706f8d7997dd
yifengdu <y.f.du1994@gmail.com>
2019-08-02 15:47:52 -0700

    Update NV_NVDLA_MCIF_READ_IG_bpt.scala

--- src/main/scala/nvdla/nocif/NV_NVDLA_MCIF_READ_IG_bpt.scala
+++ src/main/scala/nvdla/nocif/NV_NVDLA_MCIF_READ_IG_bpt.scala
@@ -1,58 +1,64 @@
-package nvdla
-import chisel3._
-import chisel3.experimental._
-import chisel3.util._
+// package nvdla
+// import chisel3._
+// import chisel3.experimental._
+// import chisel3.util._
 
-class NV_NVDLA_MCIF_READ_IG_bpt(implicit conf: cscConfiguration) extends Module {
-    val io = IO(new Bundle{
-        //general clock
-        val nvdla_core_clk = Input(Clock())
+// class NV_NVDLA_MCIF_READ_IG_bpt(implicit conf: cscConfiguration) extends Module {
+//     val io = IO(new Bundle{
+//         //general clock
+//         val nvdla_core_clk = Input(Clock())
 
-        //dma2bpt
-        val dma2bpt_req_valid = Input(Bool())
-        val dma2bpt_req_ready = Output(Bool())
-        val dma2bpt_req_pd = Input(UInt(conf.NVDLA_DMA_RD_REQ.W))
-        val dma2bpt_cdt_lat_fifo_pop = Input(Bool())
+//         //dma2bpt
+//         val dma2bpt_req_valid = Input(Bool())
+//         val dma2bpt_req_ready = Output(Bool())
+//         val dma2bpt_req_pd = Input(UInt(conf.NVDLA_DMA_RD_REQ.W))
+//         val dma2bpt_cdt_lat_fifo_pop = Input(Bool())
 
-        //bpt2arb
-        val bpt2arb_req_valid = Output(Bool())
-        val bpt2arb_req_ready = Input(Bool())
-        val bpt2arb_req_pd = Output(UInt(conf.NVDLA_DMA_RD_IG_PW.W))
-        val tieoff_axid = Input(UInt(3.W))
-        val tieoff_lat_fifo_depth = Input(UInt(8.W))
+//         //bpt2arb
+//         val bpt2arb_req_valid = Output(Bool())
+//         val bpt2arb_req_ready = Input(Bool())
+//         val bpt2arb_req_pd = Output(UInt(conf.NVDLA_DMA_RD_IG_PW.W))
+//         val tieoff_axid = Input(UInt(3.W))
+//         val tieoff_lat_fifo_depth = Input(UInt(8.W))
 
-    })
-}
+//     })
+//     withClock(io.nvdla_core_clk){
+// }
+// }
 
-class NV_NVDLA_MCIF_READ_IG_BPT_pipe_p1(implicit conf: cscConfiguration) extends Module {
-    val io = IO(new Bundle{
-        //general clocl
-        val nvdla_core_clk = Input(Clock())
+// class NV_NVDLA_MCIF_READ_IG_BPT_pipe_p1(implicit conf: cscConfiguration) extends Module {
+//     val io = IO(new Bundle{
+//         //general clocl
+//         val nvdla_core_clk = Input(Clock())
         
-        //dma2bpt_
-        val dma2bpt_req_pd = Input(UInt(conf.NVDLA_DMA_RD_REQ.W))
-        val dma2bpt_req_valid = Input(Bool())
-        val dma2bpt_req_ready = Output(Bool())
+//         //dma2bpt_
+//         val dma2bpt_req_pd = Input(UInt(conf.NVDLA_DMA_RD_REQ.W))
+//         val dma2bpt_req_valid = Input(Bool())
+//         val dma2bpt_req_ready = Output(Bool())
         
-        //in
-        val in_pd_p = Output(UInt(conf.NVDLA_DMA_RD_REQ.W))
-        val in_vld_p = Output(Bool())
-        val in_rdy_p = Input(Bool())
-    })
-}
+//         //in
+//         val in_pd_p = Output(UInt(conf.NVDLA_DMA_RD_REQ.W))
+//         val in_vld_p = Output(Bool())
+//         val in_rdy_p = Input(Bool())
+//     })
+//     withClock(io.nvdla_core_clk){
+// }
+// }
 
-class NV_NVDLA_MCIF_READ_IG_BPT_pipe_p2(implicit conf: cscConfiguration) extends Module {
-    val io = IO(new Bundle{
-        //general clocl
-        val nvdla_core_clk = Input(Clock())
+// class NV_NVDLA_MCIF_READ_IG_BPT_pipe_p2(implicit conf: cscConfiguration) extends Module {
+//     val io = IO(new Bundle{
+//         //general clocl
+//         val nvdla_core_clk = Input(Clock())
 
-        //in
-        val in_pd_p = Input(UInt(conf.NVDLA_DMA_RD_REQ.W))
-        val in_vld_p = Input(Bool())
-        val in_rdy_p = Output(Bool())
-        val in_pd = Output(UInt(conf.NVDLA_DMA_RD_REQ.W))
-        val in_vld = Output(Bool())
-        val in_rdy = Input(Bool())
+//         //in
+//         val in_pd_p = Input(UInt(conf.NVDLA_DMA_RD_REQ.W))
+//         val in_vld_p = Input(Bool())
+//         val in_rdy_p = Output(Bool())
+//         val in_pd = Output(UInt(conf.NVDLA_DMA_RD_REQ.W))
+//         val in_vld = Output(Bool())
+//         val in_rdy = Input(Bool())
 
-    })
-}
+//     })
+
+// withClock(io.nvdla_core_clk){
+// }}
