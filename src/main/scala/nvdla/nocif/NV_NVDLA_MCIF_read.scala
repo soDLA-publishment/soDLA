261273e6ca8505a3dafa3af59e67b2d9cd57c784
yifengdu <y.f.du1994@gmail.com>
2019-08-08 06:46:07 -0500

    IO update

--- src/main/scala/nvdla/nocif/NV_NVDLA_MCIF_read.scala
+++ src/main/scala/nvdla/nocif/NV_NVDLA_MCIF_read.scala
@@ -3,25 +3,15 @@ import chisel3._
 import chisel3.experimental._
 import chisel3.util._
 
-class NV_NVDLA_MCIF_READ_eg(implicit conf: cscConfiguration) extends Module {
+class NV_NVDLA_MCIF_READ_IG_cvt(implicit conf: cscConfiguration) extends Module {
     val io = IO(new Bundle{
         //general clock
         val nvdla_core_clk = Input(Clock())
-
-        //RDMA_NAME mcif2rd
-
-        //pwrbus
+        
         val pwrbus_ram_pd = Input(UInt(32.W))
-        
-        val eg2ig_axi_vld = Output(Bool())
-        
-        //if(NVDLA_PRIMARY_MEMIF_WIDTH > NVDLA_MEMORY_ATOMIC_WIDTH)
-        //cq_rd
-        val cq_rd_pvld = Input(Vec(conf.RDMA_NUM, Bool()))
-        val cq_rd_prdy = Output(Vec(conf.RDMA_NUM, Bool()))
-        val cq_rd_pd = Input(Vec(conf.RDMA_NUM, UInt(7.W)))
+        val reg2dp_rd_os_cnt = Input(UInt(8.W))
 
-        //RDMA_NAME mcif2rd_rsp
+        //RDMA_NAME
 
         //noc2mcif
         val noc2mcif_axi_r_rvalid = Input(Bool())
@@ -29,9 +19,13 @@ class NV_NVDLA_MCIF_READ_eg(implicit conf: cscConfiguration) extends Module {
         val noc2mcif_axi_r_rid = Input(UInt(8.W))
         val noc2mcif_axi_r_rlast = Input(Bool())
         val noc2mcif_axi_r_rdata = Input(UInt(conf.NVDLA_PRIMARY_MEMIF_WIDTH.W))
+        
+        //mcif2noc
+        val mcif2noc_axi_ar_arvalid = Output(Bool())
+        val mcif2noc_axi_ar_arready = Input(Bool())
+        val mcif2noc_axi_ar_arid = Output(UInt(8.W))
+        val mcif2noc_axi_ar_arlen = Output(UInt(4.W))
+        val mcif2noc_axi_ar_araddr = Output(UInt(conf.NVDLA_MEM_ADDRESS_WIDTH.W))
         })
     withClock(io.nvdla_core_clk){
-}}
-
-
-
+}}   
