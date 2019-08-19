bdb3bc2de953e5f40836ab9a6ce19e4c1b7c8182
redpanda3 <yuda@berkeley.edu>
2019-08-18 17:02:12 -0700

    Update NV_NVDLA_NOCIF_config.scala

--- src/main/scala/nvdla/nocif/NV_NVDLA_NOCIF_config.scala
+++ src/main/scala/nvdla/nocif/NV_NVDLA_NOCIF_config.scala
@@ -2,8 +2,16 @@ package nvdla
 
 
 class nocifConfiguration extends project_spec{
-  val WDMA_NUM = 5
-  val RDMA_NUM = 10
+
+  val RDMA_MAX_NUM = 10
+  val WDMA_MAX_NUM = 5
+  val WDMA_NUM = 8
+  val RDMA_NUM = 5
   val FV_RAND_WR_PAUSE = false
+
+  val NVDLA_DMA_RD_IG_PW = NVDLA_MEM_ADDRESS_WIDTH+11
+  val NVDLA_DMA_WR_IG_PW = NVDLA_MEM_ADDRESS_WIDTH+13
+
+  val NVDLA_PRIMARY_MEMIF_STRB = NVDLA_MEMORY_ATOMIC_SIZE
 }
 
