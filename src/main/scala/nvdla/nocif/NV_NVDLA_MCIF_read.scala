822d90f7a3ce8468176e0439c1abb9ec5de5f764
Hui <58934379@qq.com>
2019-08-20 11:31:35 +0800

    update  NV_NVDLA_MCIF_READ_ig & NV_NVDLA_MCIF_READ_eg

--- src/main/scala/nvdla/nocif/NV_NVDLA_MCIF_read.scala
+++ src/main/scala/nvdla/nocif/NV_NVDLA_MCIF_read.scala
@@ -128,6 +128,154 @@ class NV_NVDLA_MCIF_read(implicit conf: nocifConfiguration) extends Module {
         val mcif2noc_axi_ar_araddr = Output(UInt(conf.NVDLA_MEM_ADDRESS_WIDTH.W))
     })
 
-    withClock(io.nvdla_core_clk){
+    val eg2ig_axi_vld = Bool()
+
+    val u_ig = Module(new NV_NVDLA_MCIF_READ_ig)
+    u_ig.io.nvdla_core_clk      := io.nvdla_core_clk
+    u_ig.io.nvdla_core_rstn     := io.nvdla_core_rstn
+    u_ig.io.pwrbus_ram_pd       := io.pwrbus_ram_pd
+    u_ig.io.reg2dp_rd_os_cnt    := io.reg2dp_rd_os_cnt
+
+    // cdma_dat
+    u_ig.io.cdma_dat2mcif_rd_cdt_lat_fifo_pop  := io.cdma_dat2mcif_rd_cdt_lat_fifo_pop
+    u_ig.io.cdma_dat2mcif_rd_req_valid         := io.cdma_dat2mcif_rd_req_valid
+    io.cdma_dat2mcif_rd_req_ready              :=  u_ig.io.cdma_dat2mcif_rd_req_ready
+    u_ig.io.cdma_dat2mcif_rd_req_pd            := io.cdma_dat2mcif_rd_req_pd
+    // cdma_wt
+    u_ig.io.cdma_wt2mcif_rd_cdt_lat_fifo_pop  := io.cdma_wt2mcif_rd_cdt_lat_fifo_pop
+    u_ig.io.cdma_wt2mcif_rd_req_valid         := io.cdma_wt2mcif_rd_req_valid
+    io.cdma_wt2mcif_rd_req_ready              :=  u_ig.io.cdma_wt2mcif_rd_req_ready
+    u_ig.io.cdma_wt2mcif_rd_req_pd            := io.cdma_wt2mcif_rd_req_pd
+    // sdp
+    u_ig.io.sdp2mcif_rd_cdt_lat_fifo_pop  := io.sdp2mcif_rd_cdt_lat_fifo_pop
+    u_ig.io.sdp2mcif_rd_req_valid         := io.sdp2mcif_rd_req_valid
+    io.sdp2mcif_rd_req_ready              :=  u_ig.io.sdp2mcif_rd_req_ready
+    u_ig.io.sdp2mcif_rd_req_pd            := io.sdp2mcif_rd_req_pd
+
+
+    if(conf.NVDLA_SDP_BS_ENABLE) {
+        u_ig.io.sdp_b2mcif_rd_cdt_lat_fifo_pop.get  := io.sdp_b2mcif_rd_cdt_lat_fifo_pop.get
+        u_ig.io.sdp_b2mcif_rd_req_valid.get         := io.sdp_b2mcif_rd_req_valid.get
+        io.sdp_b2mcif_rd_req_ready.get              := u_ig.io.sdp_b2mcif_rd_req_ready.get
+        u_ig.io.sdp_b2mcif_rd_req_pd.get            := io.sdp_b2mcif_rd_req_pd.get
     }
+
+    if(conf.NVDLA_SDP_BN_ENABLE) {
+        u_ig.io.sdp_n2mcif_rd_cdt_lat_fifo_pop.get  := io.sdp_n2mcif_rd_cdt_lat_fifo_pop.get
+        u_ig.io.sdp_n2mcif_rd_req_valid.get         := io.sdp_n2mcif_rd_req_valid.get
+        io.sdp_n2mcif_rd_req_ready.get              := u_ig.io.sdp_n2mcif_rd_req_ready.get
+        u_ig.io.sdp_n2mcif_rd_req_pd.get            := io.sdp_n2mcif_rd_req_pd.get
+    }
+
+    if(conf.NVDLA_SDP_EW_ENABLE) {
+        u_ig.io.sdp_e2mcif_rd_cdt_lat_fifo_pop.get  := io.sdp_e2mcif_rd_cdt_lat_fifo_pop.get
+        u_ig.io.sdp_e2mcif_rd_req_valid.get         := io.sdp_e2mcif_rd_req_valid.get
+        io.sdp_e2mcif_rd_req_ready.get              := u_ig.io.sdp_e2mcif_rd_req_ready.get
+        u_ig.io.sdp_e2mcif_rd_req_pd.get            := io.sdp_e2mcif_rd_req_pd.get
+    }
+
+    if(conf.NVDLA_PDP_ENABLE) {
+        u_ig.io.pdp2mcif_rd_cdt_lat_fifo_pop.get  := io.pdp2mcif_rd_cdt_lat_fifo_pop.get
+        u_ig.io.pdp2mcif_rd_req_valid.get         := io.pdp2mcif_rd_req_valid.get
+        io.pdp2mcif_rd_req_ready.get              := u_ig.io.pdp2mcif_rd_req_ready.get
+        u_ig.io.pdp2mcif_rd_req_pd.get            := io.pdp2mcif_rd_req_pd.get
+    }
+
+    if(conf.NVDLA_CDP_ENABLE) {
+        u_ig.io.cdp2mcif_rd_cdt_lat_fifo_pop.get  := io.cdp2mcif_rd_cdt_lat_fifo_pop.get
+        u_ig.io.cdp2mcif_rd_req_valid.get         := io.cdp2mcif_rd_req_valid.get
+        io.cdp2mcif_rd_req_ready.get              := u_ig.io.cdp2mcif_rd_req_ready.get
+        u_ig.io.cdp2mcif_rd_req_pd.get            := io.cdp2mcif_rd_req_pd.get
+    }
+
+    if(conf.NVDLA_RUBIK_ENABLE) {
+        u_ig.io.rubik2mcif_rd_cdt_lat_fifo_pop.get  := io.rubik2mcif_rd_cdt_lat_fifo_pop.get
+        u_ig.io.rubik2mcif_rd_req_valid.get         := io.rubik2mcif_rd_req_valid.get
+        io.rubik2mcif_rd_req_ready.get              := u_ig.io.rubik2mcif_rd_req_ready.get
+        u_ig.io.rubik2mcif_rd_req_pd.get            := io.rubik2mcif_rd_req_pd.get
+    }
+
+    if(conf.NVDLA_BDMA_ENABLE) {
+        u_ig.io.bdma2mcif_rd_cdt_lat_fifo_pop.get  := io.bdma2mcif_rd_cdt_lat_fifo_pop.get
+        u_ig.io.bdma2mcif_rd_req_valid.get         := io.bdma2mcif_rd_req_valid.get
+        io.bdma2mcif_rd_req_ready.get              := u_ig.io.bdma2mcif_rd_req_ready.get
+        u_ig.io.bdma2mcif_rd_req_pd.get            := io.bdma2mcif_rd_req_pd.get
+    }
+
+    u_ig.io.eg2ig_axi_vld          := eg2ig_axi_vld
+    io.mcif2noc_axi_ar_arvalid     := u_ig.io.mcif2noc_axi_ar_arvalid
+    u_ig.io.mcif2noc_axi_ar_arready:= io.mcif2noc_axi_ar_arready
+    io.mcif2noc_axi_ar_arid        := u_ig.io.mcif2noc_axi_ar_arid
+    io.mcif2noc_axi_ar_arlen       := u_ig.io.mcif2noc_axi_ar_arlen
+    io.mcif2noc_axi_ar_araddr      := u_ig.io.mcif2noc_axi_ar_araddr
+
+
+    val u_eg = Module(new NV_NVDLA_MCIF_READ_eg)
+    u_eg.io.nvdla_core_clk      := io.nvdla_core_clk
+    u_eg.io.nvdla_core_rstn     := io.nvdla_core_rstn
+    u_eg.io.pwrbus_ram_pd       := io.pwrbus_ram_pd
+    eg2ig_axi_vld               := u_eg.io.eg2ig_axi_vld
+
+    // cdma_dat
+    io.mcif2cdma_dat_rd_rsp_valid       :=  u_eg.io.mcif2cdma_dat_rd_rsp_valid
+    u_eg.io.mcif2cdma_dat_rd_rsp_ready  := io.mcif2cdma_dat_rd_rsp_ready
+    io.mcif2cdma_dat_rd_rsp_pd          :=  u_eg.io.mcif2cdma_dat_rd_rsp_pd
+
+    // cdma_wt
+    io.mcif2cdma_wt_rd_rsp_valid       :=  u_eg.io.mcif2cdma_wt_rd_rsp_valid
+    u_eg.io.mcif2cdma_wt_rd_rsp_ready  := io.mcif2cdma_wt_rd_rsp_ready
+    io.mcif2cdma_wt_rd_rsp_pd          :=  u_eg.io.mcif2cdma_wt_rd_rsp_pd
+
+    // sdp
+    io.mcif2sdp_rd_rsp_valid       :=  u_eg.io.mcif2sdp_rd_rsp_valid
+    u_eg.io.mcif2sdp_rd_rsp_ready  := io.mcif2sdp_rd_rsp_ready
+    io.mcif2sdp_rd_rsp_pd          :=  u_eg.io.mcif2sdp_rd_rsp_pd
+
+    if(conf.NVDLA_SDP_BS_ENABLE) {
+        io.mcif2sdp_b_rd_rsp_valid.get       :=  u_eg.io.mcif2sdp_b_rd_rsp_valid.get
+        u_eg.io.mcif2sdp_b_rd_rsp_ready.get  := io.mcif2sdp_b_rd_rsp_ready.get
+        io.mcif2sdp_b_rd_rsp_pd.get          :=  u_eg.io.mcif2sdp_b_rd_rsp_pd.get
+    }
+
+    if(conf.NVDLA_SDP_BN_ENABLE) {
+        io.mcif2sdp_n_rd_rsp_valid.get       :=  u_eg.io.mcif2sdp_n_rd_rsp_valid.get
+        u_eg.io.mcif2sdp_n_rd_rsp_ready.get  := io.mcif2sdp_n_rd_rsp_ready.get
+        io.mcif2sdp_n_rd_rsp_pd.get          :=  u_eg.io.mcif2sdp_n_rd_rsp_pd.get
+    }
+
+    if(conf.NVDLA_SDP_EW_ENABLE) {
+        io.mcif2sdp_e_rd_rsp_valid.get       :=  u_eg.io.mcif2sdp_e_rd_rsp_valid.get
+        u_eg.io.mcif2sdp_e_rd_rsp_ready.get  := io.mcif2sdp_e_rd_rsp_ready.get
+        io.mcif2sdp_e_rd_rsp_pd.get          :=  u_eg.io.mcif2sdp_e_rd_rsp_pd.get
+    }
+
+    if(conf.NVDLA_PDP_ENABLE) {
+        io.mcif2pdp_rd_rsp_valid.get       :=  u_eg.io.mcif2pdp_rd_rsp_valid.get
+        u_eg.io.mcif2pdp_rd_rsp_ready.get  := io.mcif2pdp_rd_rsp_ready.get
+        io.mcif2pdp_rd_rsp_pd.get          :=  u_eg.io.mcif2pdp_rd_rsp_pd.get
+    }
+
+    if(conf.NVDLA_CDP_ENABLE) {
+        io.mcif2cdp_rd_rsp_valid.get       :=  u_eg.io.mcif2cdp_rd_rsp_valid.get
+        u_eg.io.mcif2cdp_rd_rsp_ready.get  := io.mcif2cdp_rd_rsp_ready.get
+        io.mcif2cdp_rd_rsp_pd.get          :=  u_eg.io.mcif2cdp_rd_rsp_pd.get
+    }
+
+    if(conf.NVDLA_RUBIK_ENABLE) {
+        io.mcif2rubik_rd_rsp_valid.get       :=  u_eg.io.mcif2rubik_rd_rsp_valid.get
+        u_eg.io.mcif2rubik_rd_rsp_ready.get  := io.mcif2rubik_rd_rsp_ready.get
+        io.mcif2rubik_rd_rsp_pd.get          :=  u_eg.io.mcif2rubik_rd_rsp_pd.get
+    }
+
+    if(conf.NVDLA_BDMA_ENABLE) {
+        io.mcif2bdma_rd_rsp_valid.get       :=  u_eg.io.mcif2bdma_rd_rsp_valid.get
+        u_eg.io.mcif2bdma_rd_rsp_ready.get  := io.mcif2bdma_rd_rsp_ready.get
+        io.mcif2bdma_rd_rsp_pd.get          :=  u_eg.io.mcif2bdma_rd_rsp_pd.get
+    }
+
+    u_eg.io.noc2mcif_axi_r_rvalid    := io.noc2mcif_axi_r_rvalid
+    io.noc2mcif_axi_r_rready         := u_eg.io.noc2mcif_axi_r_rready
+    u_eg.io.noc2mcif_axi_r_rid       := io.noc2mcif_axi_r_rid
+    u_eg.io.noc2mcif_axi_r_rlast     := io.noc2mcif_axi_r_rlast
+    u_eg.io.noc2mcif_axi_r_rdata     := io.noc2mcif_axi_r_rdata
 }
