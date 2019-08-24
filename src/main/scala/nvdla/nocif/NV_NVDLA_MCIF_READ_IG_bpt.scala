6cd7de11ec1c42a646099a08d3b06b3d6f11fe74
Hui <58934379@qq.com>
2019-08-24 21:06:46 +0800

    refactor code

--- src/main/scala/nvdla/nocif/NV_NVDLA_MCIF_READ_IG_bpt.scala
+++ src/main/scala/nvdla/nocif/NV_NVDLA_MCIF_READ_IG_bpt.scala
@@ -24,24 +24,24 @@ class NV_NVDLA_MCIF_READ_IG_bpt(implicit conf: nocifConfiguration) extends Modul
     })
 
 
-    val in_pd_p  = Wire(UInt(conf.NVDLA_DMA_RD_REQ.W))
-    val in_vld_p = Wire(Bool())
+//    val in_pd_p  = Wire(UInt(conf.NVDLA_DMA_RD_REQ.W))
+//    val in_vld_p = Wire(Bool())
     val in_rdy_p = Wire(Bool())
 
-    val in_pd   = Wire(UInt(conf.NVDLA_DMA_RD_REQ.W))
-    val in_vld  = Wire(Bool())
+//    val in_pd   = Wire(UInt(conf.NVDLA_DMA_RD_REQ.W))
+//    val in_vld  = Wire(Bool())
     val in_rdy  = Wire(Bool())
 
     //////////////////////////////////////// pipe1
     val pipe_p1 = Module(new NV_NVDLA_IS_pipe(conf.NVDLA_DMA_RD_REQ))
     pipe_p1.io.clk  := io.nvdla_core_clk
-    pipe_p1.io.di   := io.dma2bpt_req_pd
     pipe_p1.io.vi   := io.dma2bpt_req_valid
-    io.dma2bpt_req_ready := pipe_p1.io.ri
+    pipe_p1.io.di   := io.dma2bpt_req_pd
+    io.dma2bpt_req_ready := pipe_p1.io.ro
 
-    in_pd_p     := pipe_p1.io.dout
-    in_vld_p    := pipe_p1.io.vo
-    pipe_p1.io.ro := in_rdy_p
+    pipe_p1.io.ri   := in_rdy_p
+    val in_pd_p  = pipe_p1.io.dout
+    val in_vld_p = pipe_p1.io.vo
 
 
     //////////////////////////////////////// pipe2
@@ -49,11 +49,12 @@ class NV_NVDLA_MCIF_READ_IG_bpt(implicit conf: nocifConfiguration) extends Modul
     pipe_p2.io.clk  := io.nvdla_core_clk
     pipe_p2.io.di   := in_pd_p
     pipe_p2.io.vi   := in_vld_p
-    in_rdy_p        := pipe_p2.io.ri
+    in_rdy_p        := pipe_p2.io.ro
 
-    in_pd  := pipe_p2.io.dout
-    in_vld := pipe_p2.io.vo
-    pipe_p2.io.ro := in_rdy
+    pipe_p2.io.ri   := in_rdy
+    val in_pd   = pipe_p2.io.dout
+    val in_vld  = pipe_p2.io.vo
+
 
 
     withClock(io.nvdla_core_clk) {
@@ -62,62 +63,51 @@ class NV_NVDLA_MCIF_READ_IG_bpt(implicit conf: nocifConfiguration) extends Modul
         val out_size_tmp = Reg(UInt(3.W))
         val beat_size = Wire(UInt(2.W))
         val bpt2arb_accept = Wire(Bool())
-        val bpt2arb_addr = Wire(UInt(conf.NVDLA_MEM_ADDRESS_WIDTH.W))
-        val bpt2arb_axid = Wire(UInt(4.W))
-        val bpt2arb_ftran = Wire(Bool())
-        val bpt2arb_ltran = Wire(Bool())
-        val bpt2arb_odd = Wire(Bool())
-        val bpt2arb_size = Wire(UInt(3.W))
-        val bpt2arb_swizzle = Wire(Bool())
 
         val stt_offset = Wire(UInt(conf.NVDLA_MCIF_BURST_SIZE_LOG2.W))
         val end_offset = Wire(UInt(conf.NVDLA_MCIF_BURST_SIZE_LOG2.W))
-        val size_offset = Wire(UInt(conf.NVDLA_MCIF_BURST_SIZE_LOG2.W))
-        val ftran_size_tmp = Wire(UInt(conf.NVDLA_MCIF_BURST_SIZE_LOG2.W))
-        val ltran_size_tmp = Wire(UInt(conf.NVDLA_MCIF_BURST_SIZE_LOG2.W))
         val mon_end_offset_c = Wire(Bool())
 
         val ftran_size = Wire(UInt(3.W))
         val ltran_size = Wire(UInt(3.W))
         val mtran_num = Wire(UInt(conf.NVDLA_DMA_RD_SIZE.W))
 
-        val in_addr = Wire(UInt(conf.NVDLA_MEM_ADDRESS_WIDTH.W))
-        val in_size = Wire(UInt(conf.NVDLA_DMA_RD_SIZE.W))
 
-        val in_vld_pd  = Wire(UInt(conf.NVDLA_DMA_RD_REQ.W))
-
-        val is_ftran = Wire(Bool())
-        val is_ltran = Wire(Bool())
-        val is_mtran = Wire(Bool())
         val is_single_tran = Wire(Bool())
         val mon_out_beats_c = Wire(Bool())
 
-        val out_inc = Wire(Bool())
         val out_odd = Wire(Bool())
         val out_swizzle = Wire(Bool())
-        val req_enable  = Wire(Bool())
-        val req_rdy  = Wire(Bool())
-        val req_vld  = Wire(Bool())
 
-        in_rdy := req_rdy & is_ltran;
+        val count_req = RegInit(UInt(conf.NVDLA_DMA_RD_SIZE.W), 0.U)
+        val req_num = Reg(UInt(conf.NVDLA_DMA_RD_SIZE.W))
+        val lat_adv = Reg(Bool())
+        val lat_cnt_cur = RegInit(UInt(9.W), 0.U)
+        val lat_count_cnt = RegInit(UInt(9.W), 0.U)
+        val lat_count_dec = RegInit(UInt(1.W), 0.U)
 
-        in_vld_pd := Fill(conf.NVDLA_DMA_RD_REQ, in_vld) & in_pd
-        in_addr(conf.NVDLA_MEM_ADDRESS_WIDTH -1, 0) := in_vld_pd(conf.NVDLA_MEM_ADDRESS_WIDTH-1, 0)
-        in_size(conf.NVDLA_DMA_RD_SIZE-1, 0) := in_vld_pd(conf.NVDLA_DMA_RD_REQ-1, conf.NVDLA_MEM_ADDRESS_WIDTH)
+        val lat_fifo_free_slot = Wire(UInt(9.W))
+        val mon_lat_fifo_free_slot_c = Wire(Bool())
+
+        val slot_needed = Reg(UInt(3.W))
+
+
+        val in_vld_pd = Fill(conf.NVDLA_DMA_RD_REQ, in_vld) & in_pd
+        val in_addr = in_vld_pd(conf.NVDLA_MEM_ADDRESS_WIDTH-1, 0)
+        val in_size = in_vld_pd(conf.NVDLA_DMA_RD_REQ-1, conf.NVDLA_MEM_ADDRESS_WIDTH)
 
         if(conf.NVDLA_MCIF_BURST_SIZE > 1) {
-            stt_offset(conf.NVDLA_MCIF_BURST_SIZE_LOG2-1, 0) := in_addr(conf.NVDLA_MEMORY_ATOMIC_LOG2+conf.NVDLA_MCIF_BURST_SIZE_LOG2-1, conf.NVDLA_MEMORY_ATOMIC_LOG2)
-            size_offset(conf.NVDLA_MCIF_BURST_SIZE_LOG2-1,0) := in_size(conf.NVDLA_MCIF_BURST_SIZE_LOG2-1, 0)
+            val stt_offset  = in_addr(conf.NVDLA_MEMORY_ATOMIC_LOG2+conf.NVDLA_MCIF_BURST_SIZE_LOG2-1, conf.NVDLA_MEMORY_ATOMIC_LOG2)
+            val size_offset = in_size(conf.NVDLA_MCIF_BURST_SIZE_LOG2-1, 0)
             Cat(mon_end_offset_c, end_offset(conf.NVDLA_MCIF_BURST_SIZE_LOG2-1, 0)) := Cat(stt_offset, size_offset)
-
 
             is_single_tran := Mux((stt_offset + in_size) < conf.NVDLA_MCIF_BURST_SIZE.U , true.B, false.B)
 
-            ftran_size_tmp(conf.NVDLA_MCIF_BURST_SIZE_LOG2-1, 0) := Mux(is_single_tran , size_offset ,(conf.NVDLA_MCIF_BURST_SIZE-1).U - stt_offset)
-            ltran_size_tmp(conf.NVDLA_MCIF_BURST_SIZE_LOG2-1, 0) := Mux(is_single_tran , 0.U, end_offset)
+            val ftran_size_tmp = Mux(is_single_tran , size_offset ,(conf.NVDLA_MCIF_BURST_SIZE-1).U - stt_offset)
+            val ltran_size_tmp = Mux(is_single_tran , 0.U, end_offset)
 
-            ftran_size(2, 0) := Cat (Fill(3-conf.NVDLA_MCIF_BURST_SIZE_LOG2, 0.U), ftran_size_tmp)
-            ltran_size(2, 0) := Cat (Fill(3-conf.NVDLA_MCIF_BURST_SIZE_LOG2, 0.U), ltran_size_tmp)
+            ftran_size := Cat (Fill(3-conf.NVDLA_MCIF_BURST_SIZE_LOG2, 0.U), ftran_size_tmp)
+            ltran_size := Cat (Fill(3-conf.NVDLA_MCIF_BURST_SIZE_LOG2, 0.U), ltran_size_tmp)
 
             mtran_num := in_size - ftran_size - ltran_size - 1.U
         } else {
@@ -126,26 +116,9 @@ class NV_NVDLA_MCIF_READ_IG_bpt(implicit conf: nocifConfiguration) extends Modul
             mtran_num := in_size - 1.U
         }
 
-
-        val count_req = RegInit(UInt(conf.NVDLA_DMA_RD_SIZE.W), 0.U)
-        val req_num = Reg(UInt(conf.NVDLA_DMA_RD_SIZE.W))
-        val lat_fifo_stall_enable = Wire(Bool())
-        val lat_adv = Reg(Bool())
-        val lat_cnt_ext = Reg(UInt(11.W))
-        val lat_cnt_mod = Reg(UInt(11.W))
-        val lat_cnt_new = Reg(UInt(11.W))
-        val lat_cnt_nxt = Reg(UInt(11.W))
-        val lat_cnt_cur = RegInit(UInt(9.W), 0.U)
-        val lat_count_cnt = RegInit(UInt(9.W), 0.U)
-        val lat_count_dec = RegInit(UInt(1.W), 0.U)
-        val lat_count_inc = Wire(UInt(3.W))
-        val lat_fifo_free_slot = Wire(UInt(9.W))
-        val mon_lat_fifo_free_slot_c = Wire(Bool())
-
-
-
-        val slot_needed = Reg(UInt(3.W))
-
+        val is_ftran = (count_req===0.U)
+        val is_mtran = (count_req>0.U && count_req<req_num)
+        val is_ltran = (count_req===req_num)
 
         if(conf.NVDLA_MCIF_BURST_SIZE > 1) {
             when(is_ftran | is_ltran){
@@ -157,16 +130,16 @@ class NV_NVDLA_MCIF_READ_IG_bpt(implicit conf: nocifConfiguration) extends Modul
             slot_needed := 1.U(3.W)
         }
 
-        lat_fifo_stall_enable := (io.tieoff_lat_fifo_depth =/=0.U)
+        val lat_fifo_stall_enable = (io.tieoff_lat_fifo_depth =/=0.U).asBool()
         lat_count_dec := io.dma2bpt_cdt_lat_fifo_pop
-        lat_count_inc := Mux(bpt2arb_accept && lat_fifo_stall_enable , slot_needed, 0.U)
+        val lat_count_inc = Mux(bpt2arb_accept && lat_fifo_stall_enable , slot_needed, 0.U)
         lat_adv := lat_count_inc =/= Cat(0.U(2.W), lat_count_dec)
 
         // lat cnt logic
-        lat_cnt_ext := Cat(0.U(2.W), lat_cnt_cur)
-        lat_cnt_mod := lat_cnt_cur + lat_count_inc - lat_count_dec
-        lat_cnt_new := Mux(lat_adv, lat_cnt_mod, lat_cnt_ext)
-        lat_cnt_nxt := lat_cnt_new
+        val lat_cnt_ext = Cat(0.U(2.W), lat_cnt_cur)
+        val lat_cnt_mod = lat_cnt_cur + lat_count_inc - lat_count_dec
+        val lat_cnt_new = Mux(lat_adv, lat_cnt_mod, lat_cnt_ext)
+        val lat_cnt_nxt = lat_cnt_new
 
         // lat flops
         lat_cnt_cur := lat_cnt_nxt
@@ -175,7 +148,7 @@ class NV_NVDLA_MCIF_READ_IG_bpt(implicit conf: nocifConfiguration) extends Modul
 
         Cat(mon_lat_fifo_free_slot_c, lat_fifo_free_slot) := io.tieoff_lat_fifo_depth - lat_count_cnt
 
-        req_enable := (!lat_fifo_stall_enable) || (Cat(0.U(6.W), slot_needed) <= lat_fifo_free_slot)
+        val req_enable = (!lat_fifo_stall_enable) || (Cat(0.U(6.W), slot_needed) <= lat_fifo_free_slot)
 
         //================
         // bsp out: swizzle
@@ -210,7 +183,7 @@ class NV_NVDLA_MCIF_READ_IG_bpt(implicit conf: nocifConfiguration) extends Modul
             //================
             // bsp out: USER: SIZE
             //================
-            out_inc := is_ftran & is_ltran & out_swizzle && !out_odd
+            val out_inc = is_ftran & is_ltran & out_swizzle && !out_odd
             Cat(mon_out_beats_c, beat_size) := out_size(2, 1) + out_inc
         }
 
@@ -250,32 +223,22 @@ class NV_NVDLA_MCIF_READ_IG_bpt(implicit conf: nocifConfiguration) extends Modul
             req_num := in_size
         }
 
+        val bpt2arb_addr = Mux(is_ftran, in_addr, out_addr)
+        val bpt2arb_size = out_size
+        val bpt2arb_swizzle = out_swizzle
+        val bpt2arb_odd   = out_odd
+        val bpt2arb_ltran = is_ltran
+        val bpt2arb_ftran = is_ftran
+        val bpt2arb_axid  = io.tieoff_axid
 
-        is_ftran := (count_req===0.U)
-        is_mtran := (count_req>0.U && count_req<req_num)
-        is_ltran := (count_req===req_num)
+        val req_rdy = req_enable & io.bpt2arb_req_ready
+        val req_vld = req_enable & in_vld;
 
-        bpt2arb_addr := Mux(is_ftran, in_addr, out_addr)
-        bpt2arb_size := out_size
-        bpt2arb_swizzle := out_swizzle
-        bpt2arb_odd   := out_odd
-        bpt2arb_ltran := is_ltran
-        bpt2arb_ftran := is_ftran
-        bpt2arb_axid  := io.tieoff_axid
-
-        req_rdy := req_enable & io.bpt2arb_req_ready
-        req_vld := req_enable & in_vld;
+        in_rdy := req_rdy & is_ltran;
 
         io.bpt2arb_req_valid := req_vld
         bpt2arb_accept := io.bpt2arb_req_valid & req_rdy
-
-        io.bpt2arb_req_pd(3, 0) :=  bpt2arb_axid
-        io.bpt2arb_req_pd(conf.NVDLA_MEM_ADDRESS_WIDTH+3, 4) := bpt2arb_addr(conf.NVDLA_MEM_ADDRESS_WIDTH-1, 0)
-        io.bpt2arb_req_pd(conf.NVDLA_MEM_ADDRESS_WIDTH+6, conf.NVDLA_MEM_ADDRESS_WIDTH+4) := bpt2arb_size(2, 0)
-        io.bpt2arb_req_pd(conf.NVDLA_MEM_ADDRESS_WIDTH+7)  :=    bpt2arb_swizzle
-        io.bpt2arb_req_pd(conf.NVDLA_MEM_ADDRESS_WIDTH+8)  :=    bpt2arb_odd
-        io.bpt2arb_req_pd(conf.NVDLA_MEM_ADDRESS_WIDTH+9)  :=    bpt2arb_ltran
-        io.bpt2arb_req_pd(conf.NVDLA_MEM_ADDRESS_WIDTH+10) :=    bpt2arb_ftran
+        io.bpt2arb_req_pd := Cat(bpt2arb_ftran, bpt2arb_ltran, bpt2arb_odd, bpt2arb_swizzle, bpt2arb_size, bpt2arb_addr, bpt2arb_axid)
     }
 
 }
