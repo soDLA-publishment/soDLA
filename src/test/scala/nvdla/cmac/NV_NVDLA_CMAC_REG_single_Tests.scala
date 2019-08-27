6b6b4b6c5e8cddae019838c87e3947e5a82fd898
Qiwei Sun <1104439112@qq.com>
2019-08-27 18:05:19 +0800

    adjust test format

--- src/test/scala/nvdla/cmac/NV_NVDLA_CMAC_REG_single_Tests.scala
+++ src/test/scala/nvdla/cmac/NV_NVDLA_CMAC_REG_single_Tests.scala
@@ -8,38 +8,38 @@ class NV_NVDLA_CMAC_REG_single_Tests(c: NV_NVDLA_CMAC_REG_single) extends PeekPo
   val R = scala.util.Random
 
   val pointer_wren_offset = 0x7004 & 0x00000FFF
-  val status_wren_offset =  0x7000 & 0x00000FFF
+  val status_wren_offset = 0x7000 & 0x00000FFF
 
   var last_producer = false
 
-  for(i <- 0 until 100){
+  for (i <- 0 until 100) {
 
-    val offset = if(i%3 == 0){
-        R.nextInt((math.pow(2,11) - 1).toInt)
-    }else if(i%3 == 1){
+    val offset = if (i % 3 == 0) {
+      R.nextInt((math.pow(2, 11) - 1).toInt)
+    } else if (i % 3 == 1) {
       pointer_wren_offset
-    }else{
+    } else {
       status_wren_offset
     }
 
     //input
     val in_reg_offset = offset
-    val in_reg_wr_data = R.nextInt((math.pow(2,32) - 1).toInt)
+    val in_reg_wr_data = R.nextInt((math.pow(2, 32) - 1).toInt)
     val in_reg_wr_en = R.nextBoolean()
     val in_consumer = R.nextBoolean()
     val in_status_0 = R.nextInt(4)
     val in_status_1 = R.nextInt(4)
 
-    val out_producer_sw:Boolean = if(offset == pointer_wren_offset & in_reg_wr_en){
+    val out_producer_sw: Boolean = if (offset == pointer_wren_offset & in_reg_wr_en) {
       (in_reg_wr_data % 2) == 1
-    }else{
+    } else {
       peek(c.io.producer) == 1
     }
-    val out_data_out_sw:BigInt = if(offset == pointer_wren_offset){
+    val out_data_out_sw: BigInt = if (offset == pointer_wren_offset) {
       (in_consumer.toInt << 16) + out_producer_sw.toInt
-    }else if(offset == status_wren_offset){
+    } else if (offset == status_wren_offset) {
       (in_status_1 << 16) + in_status_0
-    }else{
+    } else {
       0
     }
     last_producer = out_producer_sw
@@ -54,13 +54,13 @@ class NV_NVDLA_CMAC_REG_single_Tests(c: NV_NVDLA_CMAC_REG_single) extends PeekPo
     step(1)
 
     expect(c.io.reg.rd_data, out_data_out_sw)
-    if(peek(c.io.reg.rd_data) != out_data_out_sw){
+    if (peek(c.io.reg.rd_data) != out_data_out_sw) {
       println(peek(c.io.reg.rd_data).toString)
       println(out_data_out_sw.toString)
       println("rd_data cnt" + i.toString)
     }
     expect(c.io.producer, out_producer_sw)
-    if(peek(c.io.producer) != out_producer_sw.toInt){
+    if (peek(c.io.producer) != out_producer_sw.toInt) {
       println(peek(c.io.producer).toString)
       println(out_producer_sw.toInt.toString)
       println("producer cnt" + i.toString)
@@ -70,13 +70,13 @@ class NV_NVDLA_CMAC_REG_single_Tests(c: NV_NVDLA_CMAC_REG_single) extends PeekPo
 
 }
 
-class NV_NVDLA_CMAC_REG_single_Tester extends ChiselFlatSpec{
+class NV_NVDLA_CMAC_REG_single_Tester extends ChiselFlatSpec {
   behavior of "NV_NVDLA_CMAC_REG_single"
-
-  val args = Array("-tbn", "verilator", "-tgvo", "on", "--target-dir", "test_run_dir/NV_NVDLA_CMAC_REG_single_Tester",
-  "--top-name", "NV_NVDLA_CMAC_REG_single_Tester")
-  Driver.execute(args, ()=>new NV_NVDLA_CMAC_REG_single){
-    c => new NV_NVDLA_CMAC_REG_single_Tests(c)
-  } should be(true)
-
+  "running with --generate-vcd-output on" should "create a vcd file from your test" in {
+    val args = Array("-tbn", "verilator", "-tgvo", "on", "--target-dir", "test_run_dir/NV_NVDLA_CMAC_REG_single_Tester",
+      "--top-name", "NV_NVDLA_CMAC_REG_single_Tester")
+    Driver.execute(args, () => new NV_NVDLA_CMAC_REG_single) {
+      c => new NV_NVDLA_CMAC_REG_single_Tests(c)
+    } should be(true)
+  }
 }
