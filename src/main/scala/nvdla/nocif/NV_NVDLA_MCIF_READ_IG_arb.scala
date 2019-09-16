563bd42df654aa6ca9fe9b2fc09ecd2190730173
redpanda3 <yuda@berkeley.edu>
2019-09-15 21:16:54 -0700

    Merge Patch

--- src/main/scala/nvdla/nocif/NV_NVDLA_MCIF_READ_IG_arb.scala
+++ src/main/scala/nvdla/nocif/NV_NVDLA_MCIF_READ_IG_arb.scala
@@ -5,7 +5,7 @@
  import chisel3.util._
 
 
- class NV_NVDLA_MCIF_READ_IG_arb(implicit conf:xxifConfiguration) extends Module {
+ class NV_NVDLA_MCIF_READ_IG_arb(implicit conf:nvdlaConfig) extends Module {
      val io = IO(new Bundle {
          //general clock
          val nvdla_core_clk = Input(Clock())
@@ -105,6 +105,6 @@
  }
 
  object NV_NVDLA_MCIF_READ_IG_arbDriver extends App {
-     implicit val conf: xxifConfiguration = new xxifConfiguration
+     implicit val conf: nvdlaConfig = new nvdlaConfig
      chisel3.Driver.execute(args, () => new NV_NVDLA_MCIF_READ_IG_arb())
  }
--- src/main/scala/nvdla/nocif/NV_NVDLA_MCIF_READ_ig.scala
+++ src/main/scala/nvdla/nocif/NV_NVDLA_MCIF_READ_ig.scala
@@ -3,7 +3,7 @@ import chisel3._
 import chisel3.experimental._
 import chisel3.util._
 
-class NV_NVDLA_MCIF_READ_ig (implicit conf: xxifConfiguration) extends Module {
+class NV_NVDLA_MCIF_READ_ig (implicit conf: nvdlaConfig) extends Module {
     val io = IO(new Bundle{
         //general clock
         val nvdla_core_clk = Input(Clock())
@@ -134,6 +134,6 @@ class NV_NVDLA_MCIF_READ_ig (implicit conf: xxifConfiguration) extends Module {
 }
 
 object NV_NVDLA_MCIF_READ_igDriver extends App {
-    implicit val conf: xxifConfiguration = new xxifConfiguration
+    implicit val conf: nvdlaConfig = new nvdlaConfig
     chisel3.Driver.execute(args, () => new NV_NVDLA_MCIF_READ_ig())
 }
