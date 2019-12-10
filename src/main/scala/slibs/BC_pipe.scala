9654a90c568441aac660b756353611893c66c104
redpanda3 <yuda@berkeley.edu>
2019-12-10 02:34:19 -0800

    Update BC_pipe.scala

--- src/main/scala/slibs/BC_pipe.scala
+++ src/main/scala/slibs/BC_pipe.scala
@@ -44,44 +44,20 @@ class NV_NVDLA_BC_pipe(WIDTH:Int) extends Module {
     //    do,vo,ri     
  
  withClock(io.clk){    
-//## pipe skid buffer 
-    val skid_valid = RegInit(false.B)
-    val skid_ready_flop = RegInit(true.B)
-    val skid_data = Reg(UInt(WIDTH.W))
-    val ro_out = RegInit(true.B)
-    val skid_pipe_ready = Wire(Bool())
-    val skid_catch = Wire(Bool())
-    val skid_ready = Wire(Bool())
-    val skid_pipe_valid = Wire(Bool())
-    val skid_pipe_data = Wire(UInt(WIDTH.W))
-
-    skid_catch := io.vi && skid_ready_flop && !skid_pipe_ready
-    skid_ready := Mux(skid_valid, skid_pipe_ready, !skid_catch)
-    skid_valid := Mux(skid_valid, !skid_pipe_ready, skid_catch)
-
-    skid_ready_flop := skid_ready
-    ro_out := skid_ready
-    skid_data := Mux(skid_catch, io.di, skid_data)
-
-    skid_pipe_valid := Mux(skid_ready_flop, io.vo, skid_valid)
-    skid_pipe_data := Mux(skid_ready_flop, io.di, skid_data)
-
-//## pipe valid-ready-bubble-collapse
-    val pipe_ready_bc = Wire(Bool())
+     
+    //## pipe valid-ready-bubble-collapse
     val pipe_valid = RegInit(false.B)
     val pipe_data = Reg(UInt(WIDTH.W))
     val pipe_ready = Wire(Bool())
 
-    pipe_ready_bc := pipe_ready || !pipe_valid
-    pipe_valid := Mux(pipe_ready_bc, skid_pipe_valid, true.B)
-    pipe_data := Mux(pipe_ready_bc&&skid_pipe_valid, skid_pipe_data, pipe_data)
-    skid_pipe_ready := pipe_ready_bc
+    pipe_valid := Mux(io.ro, io.vi, true.B)
+    io.ro := pipe_ready || !pipe_valid;
+    pipe_data := Mux(io.ro && io.vi, io.di, pipe_data)
 
-//## pipe (2) output
-    io.dout := pipe_data
+    //## pipe (2) output
     pipe_ready := io.ri
     io.vo := pipe_valid
-    io.ro := ro_out
+    io.dout := pipe_data
           
 }}
 
@@ -127,43 +103,19 @@ class NV_NVDLA_BC_VEC_pipe(DIM:Int, WIDTH:Int) extends Module {
     //    do,vo,ri     
  
  withClock(io.clk){    
-//## pipe skid buffer 
-    val skid_valid = RegInit(false.B)
-    val skid_ready_flop = RegInit(true.B)
-    val skid_data = Reg(Vec(DIM, UInt(WIDTH.W)))
-    val ro_out = RegInit(true.B)
-    val skid_pipe_ready = Wire(Bool())
-    val skid_catch = Wire(Bool())
-    val skid_ready = Wire(Bool())
-    val skid_pipe_valid = Wire(Bool())
-    val skid_pipe_data = Wire(Vec(DIM, UInt(WIDTH.W)))
-
-    skid_catch := io.vo && skid_ready_flop && !skid_pipe_ready
-    skid_ready := Mux(skid_valid, skid_pipe_ready, !skid_catch)
-    skid_valid := Mux(skid_valid, !skid_pipe_ready, skid_catch)
-
-    skid_ready_flop := skid_ready
-    ro_out := skid_ready
-    skid_data := Mux(skid_catch, io.di, skid_data)
-
-    skid_pipe_valid := Mux(skid_ready_flop, io.vo, skid_valid)
-    skid_pipe_data := Mux(skid_ready_flop, io.di, skid_data)
-
-//## pipe valid-ready-bubble-collapse
-    val pipe_ready_bc = Wire(Bool())
+     
+    //## pipe valid-ready-bubble-collapse
     val pipe_valid = RegInit(false.B)
-    val pipe_data = Reg(Vec(DIM, UInt(WIDTH.W)))
+    val pipe_data = Reg(UInt(WIDTH.W))
     val pipe_ready = Wire(Bool())
 
-    pipe_ready_bc := pipe_ready || !pipe_valid
-    pipe_valid := Mux(pipe_ready_bc, skid_pipe_valid, true.B)
-    pipe_data := Mux(pipe_ready_bc&&skid_pipe_valid, skid_pipe_data, pipe_data)
-    skid_pipe_ready := pipe_ready_bc
+    pipe_valid := Mux(io.ro, io.vi, true.B)
+    io.ro := pipe_ready || !pipe_valid;
+    pipe_data := Mux(io.ro && io.vi, io.di, pipe_data)
 
-//## pipe (2) output
-    io.dout := pipe_data
+    //## pipe (2) output
     pipe_ready := io.ri
     io.vo := pipe_valid
-    io.ro := ro_out
+    io.dout := pipe_data
           
 }}
