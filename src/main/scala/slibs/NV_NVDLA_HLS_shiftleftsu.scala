057698a4d301d9e79b31905fe1fff8f1d8f76763
redpanda3 <yuda@berkeley.edu>
2019-12-10 07:44:55 -0800

    .

--- src/main/scala/slibs/NV_NVDLA_HLS_shiftleftsu.scala
+++ src/main/scala/slibs/NV_NVDLA_HLS_shiftleftsu.scala
@@ -16,25 +16,21 @@ class NV_NVDLA_HLS_shiftleftsu(IN_WIDTH:Int, OUT_WIDTH:Int, SHIFT_WIDTH:Int) ext
         val data_out = Output(UInt(OUT_WIDTH.W))
     })
         
-    val data_high = Wire(UInt(HIGH_WIDTH.W))
-    val data_shift = Wire(UInt(OUT_WIDTH.W))
-    val data_max = Wire(UInt(OUT_WIDTH.W))
-    val data_sign = Wire(Bool())
-    val left_shift_sat = Wire(Bool())
-    
-    data_sign := io.data_in(IN_WIDTH-1)
-    
-    data_high := (Cat(Fill(SHIFT_MAX, data_sign), io.data_in) << io.shift_num)(HIGH_WIDTH + OUT_WIDTH - 1, OUT_WIDTH)
-    
-    data_shift := (Cat(Fill(SHIFT_MAX, data_sign), io.data_in) << io.shift_num)(OUT_WIDTH - 1, 0)
-    
-    left_shift_sat := Cat(data_high, data_shift) =/= Fill(HIGH_WIDTH+1, data_sign)
-    
-    data_max := Mux(data_sign, Cat(true.B, Fill(OUT_WIDTH-1, false.B)), ~Cat(true.B, Fill(OUT_WIDTH-1, false.B)))
-    
+    val data_sign = io.data_in(IN_WIDTH-1)    
+
+    val data_high = (Cat(Fill(SHIFT_MAX, data_sign), io.data_in) << io.shift_num)(HIGH_WIDTH + OUT_WIDTH - 1, OUT_WIDTH)
+
+    val data_shift = (Cat(Fill(SHIFT_MAX, data_sign), io.data_in) << io.shift_num)(OUT_WIDTH - 1, 0)
+
+    val left_shift_sat = Cat(data_high, data_shift) =/= Fill(HIGH_WIDTH+1, data_sign) 
+
+    val data_max = Mux(data_sign, Cat(true.B, Fill(OUT_WIDTH-1, false.B)), ~Cat(true.B, Fill(OUT_WIDTH-1, false.B)))
+
     io.data_out := Mux(left_shift_sat, data_max, data_shift)
           
 }
+
+
 
 
 
--- src/main/scala/slibs/NV_NVDLA_HLS_shiftrightusz.scala
+++ src/main/scala/slibs/NV_NVDLA_HLS_shiftrightusz.scala
@@ -17,43 +17,29 @@ class NV_NVDLA_HLS_shiftrightusz(IN_WIDTH:Int, OUT_WIDTH:Int, FRAC_WIDTH:Int, SH
         val frac_out = Output(UInt(FRAC_WIDTH.W))
     })
         
-    val shift_num_abs = Wire(UInt(SHIFT_WIDTH.W))
-    val data_shift_l = Wire(UInt(OUT_WIDTH.W))
-    val data_high = Wire(UInt(HIGH_WIDTH.W))
-    val data_shift_r = Wire(UInt(IN_WIDTH.W))
-    val frac_shift = Wire(UInt(FRAC_WIDTH.W))
-    val data_max = Wire(UInt(OUT_WIDTH.W))
-    val left_shift_sat = Wire(Bool())
-    val right_shift_sat = Wire(Bool())
-    val shift_sign = Wire(Bool())
-
-
     //shift left
-    shift_sign := io.shift_num(SHIFT_WIDTH-1)
+    val shift_sign = io.shift_num(SHIFT_WIDTH-1)
 
-    shift_num_abs := ~io.shift_num + 1.U
+    val shift_num_abs = ~io.shift_num +& 1.U
 
-    data_high := (Cat(Fill(SHIFT_MAX, false.B), io.data_in) << shift_num_abs)(HIGH_WIDTH + OUT_WIDTH - 1, OUT_WIDTH)
+    val data_high = (Cat(Fill(SHIFT_MAX, false.B), io.data_in) << shift_num_abs)(HIGH_WIDTH + OUT_WIDTH - 1, OUT_WIDTH)
     
-    data_shift_l := (Cat(Fill(SHIFT_MAX, false.B), io.data_in) << shift_num_abs)(OUT_WIDTH - 1, 0)
+    val data_shift_l = (Cat(Fill(SHIFT_MAX, false.B), io.data_in) << shift_num_abs)(OUT_WIDTH - 1, 0)
 
-    left_shift_sat := shift_sign & (Cat(data_high, data_shift_l) =/= Fill(HIGH_WIDTH+1, false.B))
+    val left_shift_sat = shift_sign & (Cat(data_high, data_shift_l) =/= Fill(HIGH_WIDTH+1, false.B))
 
     //shift right
-    data_shift_r := (Cat(io.data_in, Fill(FRAC_WIDTH, false.B)) >> io.shift_num)(IN_WIDTH + FRAC_WIDTH -1, FRAC_WIDTH)
+    val data_shift_r = (Cat(io.data_in, Fill(FRAC_WIDTH, false.B)) >> io.shift_num)(IN_WIDTH + FRAC_WIDTH -1, FRAC_WIDTH)
 
-    frac_shift := (Cat(io.data_in, Fill(FRAC_WIDTH, false.B)) >> io.shift_num)(FRAC_WIDTH-1, 0)
+    val frac_shift = (Cat(io.data_in, Fill(FRAC_WIDTH, false.B)) >> io.shift_num)(FRAC_WIDTH-1, 0)
 
-    right_shift_sat := !shift_sign & data_shift_r(IN_WIDTH-1, OUT_WIDTH).orR
+    val right_shift_sat = !shift_sign & data_shift_r(IN_WIDTH-1, OUT_WIDTH).orR
 
-    data_max := Fill(OUT_WIDTH, true.B)
+    val data_max = Fill(OUT_WIDTH, true.B)
 
     //final out
     io.data_out := Mux(left_shift_sat | right_shift_sat, data_max, Mux(shift_sign, data_shift_l, data_shift_r(OUT_WIDTH-1, 0)))
     io.frac_out := Mux(shift_sign, Fill(FRAC_WIDTH, false.B), frac_shift)
   
 }
-
-
-
 
